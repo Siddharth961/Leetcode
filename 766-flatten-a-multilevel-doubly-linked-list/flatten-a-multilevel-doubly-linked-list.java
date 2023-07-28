8c14afb3faf9b5903bad3b836ba5957b8418d25f
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-07-28 17:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (23.16%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        if(head==null)return null;
+        Node tail = connector(head);
+        return head;
+    }
+
+    public Node connector(Node head){
+        Node i = head;
+        while(i.next!=null){
+
+            
+
+            if(i.child!=null){
+                Node j = i.next;
+                i.next = i.child;
+                i.child.prev = i;
+
+                Node ctail = connector(i.child); //getting tail of child list
+
+                ctail.next = j;
+                j.prev = ctail;
+                i.child = null;
+            }
+
+            i=i.next;
+        }
+        if(i.next==null&&i.child!=null){
+                
+                i.next = i.child;
+                i.child.prev = i;
+                i.child=null;
+                return connector(i.next);
+            }
+            
+        return i;//returning the tail
+    }
+}
