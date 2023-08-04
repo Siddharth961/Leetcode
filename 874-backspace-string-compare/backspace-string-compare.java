aa35ceda11754417ca3de518465ba15ba03b3057
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-04 19:25:07 +0530

    Time: 2 ms (63.81%) | Memory: 40.2 MB (99.32%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        StringBuilder sb = new StringBuilder(s);
+        int i=0;
+        while(i<sb.length()){
+            if(i==0 && sb.charAt(i)=='#')sb.delete(i,i+1);
+            else if(sb.charAt(i)=='#'){
+                sb.delete(i-1,i+1);
+                i--;
+            }
+            else i++;
+        }
+        String s1 = sb.toString();
+        
+        sb = new StringBuilder(t);
+         i=0;
+        while(i<sb.length()){
+            if(i==0 && sb.charAt(i)=='#')sb.delete(i,i+1);
+
+            else if(sb.charAt(i)=='#'){
+                sb.delete(i-1,i+1);
+                i--;
+            }
+            else i++;
+        }
+        String s2 = sb.toString();
+        return s1.equals(s2);
+    }
+}
