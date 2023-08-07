32f4ad3f1db25b9cb8712c2a4d7d018ae0ae825d
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-07 23:54:45 +0530

    Time: 2 ms (50.59%) | Memory: 40.1 MB (81.00%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,38 +1,23 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
 
-        if(tickets[k]==0) return 0;
-
-        int cycle = tickets[k];
-        int count = 0;
+        int i=0;
         int time=0;
+        while(tickets[k]!=0){
 
-        Queue<Integer> q = new LinkedList<>();
+            if(tickets[i]>0){
+                tickets[i]--;
+                time++;
+            }
 
-        for(int i=0; i<tickets.length; i++){
-            q.add(tickets[i]);
+            if(tickets[k]==0) return time;
+
+            i++;
+            if(i==tickets.length) i=0;
         }
 
-        while(cycle>0){
+        return time;
 
-            if(count==k && q.peek()==1)return time+1;
-
-            if(q.peek()>0){
-            q.add(q.remove()-1);
-            time++;
-            }
-            else q.add(q.remove());
-
-
-
-            count++;
-            if(count==tickets.length){
-                count=0;
-                cycle--;
-            }
-        }
-
-        return time+k+1;
 
     }
 }
