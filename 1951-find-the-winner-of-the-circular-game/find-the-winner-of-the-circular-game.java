4a631fbf12ed28f334a10a50fa4887e66e228afa
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-08 15:09:57 +0530

    Time: 43 ms (13.85%) | Memory: 43.8 MB (5.79%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,31 +1,21 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        int[]arr = new int[n+1];
+        Queue<Integer> q = new LinkedList<>();
 
         for(int i=1; i<=n; i++){
-            arr[i]=i;
+            q.add(i);
         }
 
-        int size=n;
-        int i=0;
 
-        while(size>1){
-            int count = k;
-            while(count>0){
-                
-                if(i==arr.length-1)i=1;
-                else i++;
-                if(arr[i]!=-1) count--;
+        while(q.size()>1){
+            int count = 1;
+            while(count<k){
+                q.add(q.remove());
+                count++;
             }
-            arr[i]=-1;
-            
-            size--;
-
+            q.remove();
         }
-        for( i=1; i<=n; i++){
-            if(arr[i]>-1)return arr[i];
-        }
-
-        return i;
+        
+        return q.peek();
     }
 }
