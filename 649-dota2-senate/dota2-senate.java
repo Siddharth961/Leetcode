46a3d443c11a48263bcfcc0bb0a635b3250ba869
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-12 00:34:51 +0530

    Time: 10 ms (87.46%) | Memory: 43.7 MB (76.38%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i=0; i<senate.length(); i++){
+            q.add(senate.charAt(i));
+        }
+
+        int r_count = 0;
+        int d_count = 0;
+
+        
+
+        while(r_count<q.size() && d_count<q.size()){
+            while(d_count!=0 && q.peek()=='R'){
+                q.remove();
+                d_count--;
+            }
+
+            while(r_count!=0 && q.peek()=='D'){
+                q.remove();
+                r_count--;
+            }
+
+            if(q.peek()=='R'){
+                q.add(q.remove());
+                r_count++;
+            }
+
+            else if(q.peek()=='D'){
+                q.add(q.remove());
+                d_count++;
+            }
+        }
+
+        if(d_count>r_count) return "Dire";
+        else return "Radiant";
+    }
+}
