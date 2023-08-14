b79d84b0ec57df7aac5b0edf684008909e3e7167
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-14 14:40:32 +0530

    Time: 19 ms (92.84%) | Memory: 46.1 MB (98.46%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -10,31 +10,37 @@
  */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        ListNode ptr = head;
+        ListNode i = head;
+        int idx = 0;
 
         Stack<Integer> st = new Stack<>();
-        ArrayList<Integer> al = new ArrayList<>();
 
-        while(ptr!=null){
-            al.add(ptr.val);
-            ptr = ptr.next;
+        while(i!=null){
+            i=i.next;
+            idx++;
         }
 
-        int[]arr = new int[al.size()];
-       
-       for(int i=al.size()-1; i>=0 ; i--){
+        int[]arr = new int[idx];
+        idx=0;
+        i=head;
 
-           while(st.size()!=0 && al.get(st.peek())<= al.get(i)){
-               st.pop();
+        while(i!=null){
+            while(st.size()!=0 && arr[st.peek()] < i.val ){
+                arr[st.peek()]=i.val;
+                st.pop();
             }
 
-           if(st.size()==0) arr[i] = 0;
-           else arr[i] = al.get(st.peek());
-           st.push(i);
-       }
-       
+            arr[idx] = i.val;
+            st.push(idx);
 
-        
+            i=i.next;
+            idx++;
+        }
+
+        while(st.size()!=0){
+            arr[st.peek()]=0;
+            st.pop();
+        }
 
         return arr;
     }
