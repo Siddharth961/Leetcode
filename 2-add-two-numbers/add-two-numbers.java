9d826c5076c10f6e99fb4bf3000815fb358b640c
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-16 21:02:19 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (52.79%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,36 +10,29 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ArrayList<Integer> num1 = new ArrayList<>();
-        ArrayList<Integer> num2 = new ArrayList<>();
-
-        ListNode i = l1;
-        while(i!=null){
-            num1.add(i.val);
-            i=i.next;
-        }
-
-        i=l2;
-        while(i!=null){
-            num2.add(i.val);
-            i=i.next;
-        }
+        
 
         
 
        
         ListNode head = new ListNode(-1);
-        i = head;
+        ListNode i = head;
 
-        int a = 0;
-        int b = 0;
+        ListNode a = l1;
+        ListNode b = l2;
 
         int c=0,val=0;
 
-        while(a<num1.size() || b<num2.size() || c>0){
+        while(a!=null || b!=null || c>0){
             val =  c;
-            if(a<num1.size()) val += num1.get(a);
-            if(b<num2.size()) val += num2.get(b);
+            if(a!=null){
+                val += a.val;
+                a = a.next;
+                }
+            if(b!=null){
+                val += b.val;
+                b=b.next;
+            }
             c = val/10;
             val = val%10;
             ListNode temp = new ListNode(val);
@@ -47,8 +40,6 @@ class Solution {
             i.next = temp;
             i = temp;
 
-            a++;
-            b++;
 
         }
 
