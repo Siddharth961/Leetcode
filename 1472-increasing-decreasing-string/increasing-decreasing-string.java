11ee8317802d26c097f286d3e76f9246236b498f
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-20 02:14:10 +0530

    Time: 3 ms (85.82%) | Memory: 43.6 MB (47.59%) - LeetSync

--- 1472-increasing-decreasing-string/increasing-decreasing-string.java
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.java
@@ -1,21 +1,10 @@
 class Solution {
     public String sortString(String s) {
-        TreeMap<Character, Integer> tm = new TreeMap<>();
+        int[]arr = new int[26];
 
         for(char c : s.toCharArray()){
-            if(tm.containsKey(c)==true){
-                tm.put(c, tm.get(c)+1);
-            }
-
-            else tm.put(c,1);
-        }
-
-        char[]arr = new char[tm.size()];
-        int i=0;
-
-        for(char c : tm.keySet()){
-            arr[i] = c;
-            i++;
+            arr[c-'a']++;
+            
         }       
 
       
@@ -23,16 +12,16 @@ class Solution {
 
         while(ans.length()!=s.length()){
 
-            for( i=0; i<arr.length; i++){
-                if(tm.get(arr[i])==0) continue;
-                ans.append(arr[i]);
-                tm.put(arr[i], tm.get(arr[i])-1);
+            for(int i=0; i<arr.length; i++){
+                if(arr[i]==0) continue;
+                ans.append((char)(i+'a'));
+                arr[i]--;
 
             }
-            for( i=arr.length-1; i>=0; i--){
-                if(tm.get(arr[i])==0) continue;
-                ans.append(arr[i]);
-                tm.put(arr[i], tm.get(arr[i])-1);
+            for(int i=arr.length-1; i>=0; i--){
+                if(arr[i]==0) continue;
+                ans.append((char)(i+'a'));
+                arr[i]--;
 
             }
         }
