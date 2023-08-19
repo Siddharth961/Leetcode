cb933d47f908f43254cadf74e0bd6cc7d3abaf8a
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-20 01:17:51 +0530

    Time: 3 ms (35.06%) | Memory: 41.2 MB (6.16%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.java
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -1,14 +1,16 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
         String[]alpha = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        HashMap<String,Integer>mp = new HashMap<>();
+
+
+        HashMap<String,Boolean>mp = new HashMap<>();
         for(String s : words){
             String sb = "";
             for(int i=0; i<s.length(); i++){
                 int x = s.charAt(i) - 97;
                 sb += alpha[x];
             }
-            mp.put(sb,1);
+            mp.put(sb,true);
         }
         return mp.size();
     }
