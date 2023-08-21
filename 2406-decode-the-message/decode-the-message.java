0729690e8ce713421c16384481cdb61e87f77bc5
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-22 01:01:07 +0530

    Time: 7 ms (72.67%) | Memory: 43.1 MB (77.87%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        HashMap<Character,Character> mp = new HashMap<>();
+
+        int i = 97;
+        for(char c: key.toCharArray()){
+            if(c==' ')continue;
+            if(mp.containsKey(c)==false){
+                mp.put(c, (char)i);
+                i++;
+
+            }
+
+
+            if(i==123)break; 
+
+        }
+
+        System.out.print(mp);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : message.toCharArray()){            
+            if(c==' ')sb.append(' ');
+            else sb.append(mp.get(c));
+
+        }
+
+        return sb.toString();
+    }
+}
