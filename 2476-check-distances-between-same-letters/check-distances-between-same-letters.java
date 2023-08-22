7113a717e56ff063544967a8a4179fec5ca61846
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-23 00:05:00 +0530

    Time: 27 ms (5.82%) | Memory: 44.4 MB (5.15%) - LeetSync

--- 2476-check-distances-between-same-letters/check-distances-between-same-letters.java
+++ 2476-check-distances-between-same-letters/check-distances-between-same-letters.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean checkDistances(String s, int[] distance) {
         HashMap<Character, Integer> mp = new HashMap<>();
+        int idx=0,d=0;
 
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
 
             if(mp.containsKey(c)==false) mp.put(c,i);
-            else mp.put(c, i-mp.get(c)-1);
+            else{
+                idx = (int)(c - 'a');
+                d = i - mp.get(c) - 1;
+                if(distance[idx]!=d) return false;
+            }
+            System.out.println(mp);
         }
 
-        
 
-        for(var e : mp.entrySet()){
-            int idx = (int)(e.getKey() - 'a');
-            if(distance[idx]!=e.getValue()) return false;
-        }
 
         return true;
     }
