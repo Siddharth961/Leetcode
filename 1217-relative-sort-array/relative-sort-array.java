9adbf4db7b8d99e0ed1581de5bae0386858a5915
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-23 00:36:37 +0530

    Time: 4 ms (20.93%) | Memory: 41.1 MB (74.28%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -20,13 +20,13 @@ class Solution {
             mp.remove(k);
         }
 
-        int val=0;
-        for(var e : mp.entrySet()){
-            val = e.getValue();
-            while(val>0){
-                ans[i] = e.getKey();
+        
+        for(var e : mp.keySet()){
+            
+            while(mp.get(e)>0){
+                ans[i] = e;
                 i++;
-                val--;                
+                mp.put(e, mp.get(e)-1);                                 
             }
         }
 
