281616af5e3e7668dc8ebb7747276205ac145ede
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-24 00:18:48 +0530

    Time: 26 ms (26.39%) | Memory: 43.7 MB (49.13%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public boolean isPalindrome(String s) {
+       
+        s.replaceAll("[^a-zA-Z0-9]","");
+        s.toLowerCase();
+        System.out.print(s);
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<=j){
+            char currFirst = s.charAt(i);
+        	char currLast = s.charAt(j);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		i++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		j--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		i++;
+        		j--;
+        	}
+        }
+
+        return true;
+        
+    }
+}
