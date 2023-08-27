8901cc5e638418668e35c0946400a9e868cd55ad
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-27 23:23:59 +0530

    Time: 1 ms (86.97%) | Memory: 40.7 MB (63.77%) - LeetSync

---
+++ 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        int a=0;
+        int b=0;
+
+        for(int i=0; i<word1.length(); i++){
+            a = word1.charAt(i) - 'a';
+            b = word2.charAt(i) - 'a';
+
+            arr1[a]++;
+            arr2[b]++;
+        }
+
+        for(int i=0; i<26; i++){
+            int x = Math.abs(arr1[i]-arr2[i]);
+            if(x>3) return false;
+        }
+
+        return true;
+    }
+}
