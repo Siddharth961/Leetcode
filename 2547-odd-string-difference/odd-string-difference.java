32226c9d6a9d26e2befb5d2689ba7a0d5bd90e23
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-28 20:10:55 +0530

    Time: 9 ms (14.17%) | Memory: 40.4 MB (49.58%) - LeetSync

--- 2547-odd-string-difference/odd-string-difference.java
+++ 2547-odd-string-difference/odd-string-difference.java
@@ -1,21 +1,36 @@
 class Solution {
     public String oddString(String[] words) {
-        Map<String,Integer> map=new HashMap<>();
-        Map<String,String> m=new HashMap<>();
-        for(String s:words)
-        {
-            String str="";
-            for(int i=1;i<s.length();i++){
-                str+=((s.charAt(i)-'a')-(s.charAt(i-1)-'a'));
-                str+=" ";
-            }
-            m.put(str,s);
-            map.put(str,map.getOrDefault(str,0)+1);
-        }
+        HashMap<String, String> m1 = new HashMap<>();
+        HashMap<String, Integer> m2 = new HashMap<>();
         
-        for(String s1:map.keySet()){
-            if(map.get(s1)==1)return m.get(s1);
+        for(String s : words){
+            String str = calc(s);
+
+            m1.put(str,s);
+            m2.put(str,m2.getOrDefault(str,0)+1); 
+        }
+        System.out.println(m1);
+        System.out.println(m2);
+         for(String s1:m2.keySet()){
+            if(m2.get(s1)==1)return m1.get(s1);
         }
         return "";
+        
+    }
+
+    public String calc(String s){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<s.length()-1; i++){
+            char a = s.charAt(i);
+            char b = s.charAt(i+1);
+
+            sb.append(b-a);
+            sb.append(" ");
+        }
+
+        System.out.println(sb);
+
+        return sb.toString();
     }
 }
