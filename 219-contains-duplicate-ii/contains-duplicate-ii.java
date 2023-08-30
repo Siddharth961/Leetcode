96b461a3649cba687b9a99d60c6c63e786428b52
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-08-30 20:12:25 +0530

    Time: 20 ms (54.83%) | Memory: 57 MB (27.24%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -5,9 +5,8 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             if(mp.containsKey(nums[i])==false){
                 mp.put(nums[i],i);
-
             }
-
+            
             else{
                 if(i-mp.get(nums[i])<=k) return true;
                 else if(k<nums.length/2) mp.put(nums[i],i);
