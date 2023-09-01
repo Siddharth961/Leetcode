293dfc871d838b826f1407e502808c36bcd12170
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-09-01 14:39:04 +0530

    Time: 217 ms (6.24%) | Memory: 46.5 MB (34.51%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.java
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -1,8 +1,10 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
         HashMap<Integer,Integer> mp = new HashMap<>();
+        ArrayList<Integer> al = new ArrayList<>();
         int max = 0;
         for(int i : nums){
+            al.add(i);
             if(mp.containsKey(i)) mp.put(i, mp.get(i)+1);
             else mp.put(i,1);
 
@@ -13,8 +15,9 @@ class Solution {
 
         for(var e : mp.entrySet()){
             if(e.getValue()==max){
-                int a = len(e.getKey(),nums);
-                if(ans>a) ans=a;
+                int a = al.indexOf(e.getKey());
+                int b = al.lastIndexOf(e.getKey());
+                if(ans>(b-a+1)) ans=b-a+1;
             }
         }
 
@@ -22,20 +25,6 @@ class Solution {
 
         
     }
-
-    public int len(int a,int[]nums){
-            int i=0;
-            int j=nums.length-1;
-
-            while(true){
-                if(nums[i]==a && nums[j]==a) break;
-                if(nums[i]!=a) i++;
-                if(nums[j]!=a) j--;
-            }
-
-            return j-i+1;
-        
-        }
 
     
 }
