222a6079e01fa9ca73f174153f67df92ef353ef8
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-09-01 18:21:07 +0530

    Time: 20 ms (77.46%) | Memory: 46.4 MB (39.65%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.java
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -1,23 +1,30 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        ArrayList<Integer> al = new ArrayList<>();
+        HashMap<Integer,int[]> mp = new HashMap<>();
         int max = 0;
-        for(int i : nums){
-            al.add(i);
-            if(mp.containsKey(i)) mp.put(i, mp.get(i)+1);
-            else mp.put(i,1);
+        for(int i=0; i<nums.length; i++){
+      
+            if(mp.containsKey(nums[i])){
+                int[]arr = mp.get(nums[i]);
+                arr[1] = i;
+                arr[2]++;
+                mp.put(nums[i], arr);
+            }
+            else{
+                int[]arr = new int[]{i,i,1};
+                mp.put(nums[i],arr);
+            }
 
-            if(max<mp.get(i)) max = mp.get(i);
+            if(max<mp.get(nums[i])[2]) max = mp.get(nums[i])[2];
         }
 
         int ans=nums.length;
 
         for(var e : mp.entrySet()){
-            if(e.getValue()==max){
-                int a = al.indexOf(e.getKey());
-                int b = al.lastIndexOf(e.getKey());
-                if(ans>(b-a+1)) ans=b-a+1;
+            if(e.getValue()[2]==max){
+                int[]arr = e.getValue();
+                
+                if(ans>(arr[1]-arr[0]+1)) ans=arr[1]-arr[0]+1;
             }
         }
 
