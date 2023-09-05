624170d90372e5ba2b68c97fe682fe70766ac984
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-09-05 23:30:02 +0530

    Time: 8 ms (53.47%) | Memory: 41.1 MB (67.16%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character, Integer> mp = new HashMap<>();
         ArrayList<Integer> al = new ArrayList<>();
-
-        for(int i=0; i<s.length(); i++) mp.put(s.charAt(i) , i);
         int part = 0;
         int count=0;
 
         for(int i=0; i<s.length(); i++){
-            int x = mp.get(s.charAt(i)); 
+            char c = s.charAt(i);
+            int x = s.lastIndexOf(c);
+
             if(part<x) part=x;
-            
+
             count++;
 
             if(i==part){
