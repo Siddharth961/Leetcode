bfca8a4ef694cc005dcc9a9411e2e47b9b8bebb5
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-09-05 20:21:18 +0530

    Time: 7 ms (54.73%) | Memory: 44.4 MB (72.46%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,13 +1,15 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groups) {
-        List<List<Integer>> ans = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         HashMap<Integer, List<Integer> > mp = new HashMap<>();
 
         for(int i=0; i<groups.length; i++){
             if(mp.containsKey(groups[i])==false){
-                mp.put(groups[i], new LinkedList<>());
+                List<Integer> ll = new ArrayList<>();
+                mp.put(groups[i],ll);
+                ll.add(i);
             }
-             mp.get(groups[i]).add(i);
+            else mp.get(groups[i]).add(i);
             
             if(groups[i]== mp.get(groups[i]).size() ){
                 ans.add(mp.get(groups[i]));
