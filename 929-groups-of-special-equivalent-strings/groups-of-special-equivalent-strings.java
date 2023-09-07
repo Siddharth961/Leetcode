ea8620ef30c6408134c1183eae24e1793126445c
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-09-07 19:38:17 +0530

    Time: 7 ms (64.15%) | Memory: 43.1 MB (62.26%) - LeetSync

---
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numSpecialEquivGroups(String[] words) {
+        HashSet<String> st = new HashSet<>();
+
+        for(String s : words){
+            st.add(mark(s));
+        }
+
+        return st.size();
+    }
+
+    public String mark(String s){
+        byte[][] arr = new byte[2][26];
+
+        for(int i=0; i<s.length(); i++){
+            if(i%2==0) arr[0][s.charAt(i)-'a']++;
+            else arr[1][s.charAt(i)-'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<26; i++){
+            sb.append(arr[0][i]);
+            sb.append(arr[1][i]);
+        }
+
+        return sb.toString();
+    }
+}
