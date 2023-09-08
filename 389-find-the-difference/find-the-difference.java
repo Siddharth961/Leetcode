cf10913ce8f59e3b35cbb9b6b40d3de51a21fa83
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-09-08 23:46:10 +0530

    Time: 2 ms (68.29%) | Memory: 40.7 MB (63.24%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,14 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character, Integer> mp = new HashMap<>();
+        char[] arr = new char[26];
 
         for(char c : s.toCharArray()){
-            if(mp.containsKey(c)) mp.put(c , mp.get(c)+1);
-            else mp.put(c,1);
+            arr[c-'a']++;
         }
         for(char c : t.toCharArray()){
-            if(mp.containsKey(c)==false || mp.get(c)==0) return c;
-            else mp.put(c, mp.get(c)-1);
+            if(arr[c-'a']==0) return c;
+            arr[c-'a']--;
         }
 
         return ' ';
