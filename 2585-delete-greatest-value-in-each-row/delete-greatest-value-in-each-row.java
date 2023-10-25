15734bc359a9bb16c5eeb0997c0a7d23041ed218
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-10-25 23:33:44 +0530

    Time: 9 ms (21.20%) | Memory: 42.9 MB (90.02%) - LeetSync

--- 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.java
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.java
@@ -1,6 +1,6 @@
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
-        for(int[]arr : grid) Arrays.sort(arr);
+        for(int i=0; i<grid.length; i++) Arrays.sort(grid[i]);
 
         int j = grid[0].length-1;
         int ans = 0;
