c2bc86afe28a77eab4c6e45299e4643f7a86da99
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2023-11-21 00:36:30 +0530

    Time: 14 ms (6.05%) | Memory: 44.2 MB (15.66%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,80 @@
+class Solution {
+    List<List<String>> al = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        boolean[][]board = new boolean[n][n];
+        ways(0,0,"", n,board);
+        return al;
+
+        
+    }
+
+    public void ways(int r, int qsf, String psf,int n, boolean[][]board){
+
+        if(r>=n){
+            if(qsf==n) ans(board);
+            return;
+        }
+
+        for( int c=0; c<n; c++){
+
+            if(check(board,r,c)==false) continue; //we cant place queen
+
+            board[r][c]=true;
+
+            ways(r+1, qsf+1, psf+r+c+" ", n, board);          
+
+            board[r][c]=false; 
+
+        }
+
+
+
+    } 
+
+    public boolean check(boolean[][]board, int r, int c){
+
+        //top check
+        for(int i=r; i>=0; i--){
+            if(board[i][c]==true) return false;
+        }
+
+        //left diagonal
+        int i=r;
+        int j=c;
+        while(i>=0 && j>=0){
+            if(board[i][j]==true) return false;
+            i--;
+            j--;
+        }
+
+        //right diagonal
+        i=r;
+        j=c;
+        while(i>=0 && j<board.length){
+            if(board[i][j]==true) return false;
+            i--;
+            j++;
+        }
+
+        return true;
+
+        
+    }
+
+    public void ans(boolean[][]board){
+        List<String> temp = new ArrayList<>();
+        
+
+        for(int i=0; i<board.length; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<board.length; j++){
+                if(board[i][j]==false) sb.append(".");
+                else  sb.append("Q");
+            }
+
+            temp.add(sb.toString());
+        }
+
+        al.add(temp);
+    }
+}
