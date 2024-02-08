a8c3c191ce1f4008704919641a76b9fbfff0d6d4
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-02-08 22:47:51 +0530

    Time: 131 ms (18.44%) | Memory: 54.9 MB (5.59%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numSquares(int n) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=1; i*i<=n; i++) al.add(i*i);
+
+        int[][]dp = new int[al.size()][n+1];
+
+        for(int i=0; i<al.size(); i++){
+                System.out.print(al.get(i)+"----------");
+            
+            for(int j=1; j<=n; j++){
+                if(i==0){
+                    dp[i][j] = j;
+                    continue;
+                }
+                if(j<al.get(i)) dp[i][j] = dp[i-1][j];
+                else{
+                 dp[i][j] = Math.min(dp[i][j - al.get(i)] + 1, dp[i-1][j]);
+                }    
+            }
+                
+
+        }
+
+        return dp[al.size()-1][n];
+    }
+}
