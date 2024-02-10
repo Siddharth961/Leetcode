b1d70f08d865e6cffbbdff0bed9835689313796f
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-02-10 22:51:56 +0530

    Time: 1 ms (99.83%) | Memory: 41.3 MB (71.73%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,30 +1,34 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length();
-        boolean[][] palindrome = new boolean[n][n];
-        int ans = 0;
+        int count = 0;
+        int l=0;
+        int r=0;
 
-        for(int i=0;i<n;i++) {
-            palindrome[i][i] = true;
-            ans++;
-        }
+        char[]arr = s.toCharArray();
 
-        for(int i=0;i<n-1;i++) {
-            if(s.charAt(i) == s.charAt(i+1)) {
-                palindrome[i][i+1] = true;
-                ans++;
-            }
-        }
-
-        for(int len=3;len<=n;len++) {
-            for(int i=0;i<n-len+1;i++) {
-                if(s.charAt(i) == s.charAt(i+len-1) && palindrome[i+1][i+len-2]) {
-                    palindrome[i][i+len-1] = true;
-                    ans++;
+        for(int i=0; i<arr.length; i++){
+            l=i;
+            r=i;
+            while(l>=0 && r<arr.length){
+                if(arr[l]==arr[r]){
+                    count++;
+                    l--;
+                    r++;
                 }
+                else break;
+            }
+            l=i;
+            r=i+1;
+            while(l>=0 && r<arr.length){
+                if(arr[l]==arr[r]){
+                    count++;
+                    l--;
+                    r++;
+                }
+                else break;
             }
         }
 
-        return ans;
+        return count;
     }
 }
