d16984870a0d9c6832a282886c835f74f6655e02
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-02-11 23:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (78.99%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    String[]arr = {"", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    List<String> al = new ArrayList<>();
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.length()==0) return al;
+        StringBuilder sb = new StringBuilder();
+        get_ans(0,digits,sb);
+        return al;
+    }
+
+    public void get_ans(int idx, String digits, StringBuilder sb){
+
+        if(idx >= digits.length()){
+            al.add( sb.toString());
+            return;
+        }
+
+        String s = arr[digits.charAt(idx) - '0' -1];
+        //System.out.println(s);
+
+        for(char c : s.toCharArray()){
+            sb.append(c);
+            get_ans(idx+1, digits, sb);
+            sb.deleteCharAt( sb.length()-1 );
+        }
+    }
+
+}
