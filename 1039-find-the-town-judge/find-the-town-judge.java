098265b50c3be21e2ab3ed537b4b173c22a2bbce
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-02-22 23:25:15 +0530

    Time: 2 ms (99.85%) | Memory: 53.8 MB (43.36%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,28 +1,14 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        HashSet<Integer>st = new HashSet<>();
-
-        for(int i=1; i<=n; i++) st.add(i);
-       
-        for(int i=0; i<trust.length; i++){
-            if(trust[i][0] != trust[i][1]) st.remove(trust[i][0]);
+        int[] countTrusts = new int[n+1];
+        for(int[] t : trust) {
+            countTrusts[t[0]] = -1;
+            if(countTrusts[t[1]] == -1) continue;
+            countTrusts[t[1]]++;
         }
-        int ans = -1;
-
-        for( var e : st){
-            ans = e;                    
+        for(int i=1; i <n+1; i++){
+            if(countTrusts[i] == n-1) return i;
         }
-
-        if(ans==-1) return ans;
-
-        int count=0;
-
-        for(int i=0; i<trust.length; i++){
-            if(trust[i][1] == ans) count++;
-        }
-
-        if(count==n || count == n-1) return ans;
-
         return -1;
     }
 }
