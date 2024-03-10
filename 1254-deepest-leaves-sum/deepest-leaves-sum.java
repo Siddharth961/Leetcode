95b1ae4acddb9cdf472356968e5d5caeac1ed427
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-03-10 16:11:08 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (47.16%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.java
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.java
@@ -14,29 +14,32 @@
  * }
  */
 class Solution {
+    int max = 0;
+    int ans = 0;
     public int deepestLeavesSum(TreeNode root) {
-        int[]ans = get_ans(root, 0);
-        return ans[0];
+        get_ans(root, 0);
+        return ans;
     }
 
-    public int[] get_ans(TreeNode root, int level){
+    public void get_ans(TreeNode root, int level){
+
+        if(root == null) return;
 
         if(root.left==null && root.right==null){
-            int[]arr = {root.val, level};
-            return arr;
+            if(max < level){
+                max = level;
+                ans = root.val;
+            }
+            else if( max == level) ans += root.val;
+
+            return;
         }
 
-        if(root.left==null) return get_ans(root.right, level+1);
-        if(root.right == null) return get_ans(root.left, level+1);
 
-        int[]left = get_ans(root.left, level+1);
-        int[]right = get_ans(root.right, level+1);
+        get_ans(root.left, level+1);
+        get_ans(root.right, level+1);
 
-        if(left[1] > right[1]) return left;
-        if(left[1] < right[1]) return right;
 
-        left[0] += right[0];
-
-        return left;        
+        return ;        
     }
 }
