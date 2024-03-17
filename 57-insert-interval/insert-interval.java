0d10762cd554e787e43fc31cf5228e7620b238e1
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-03-18 00:57:00 +0530

    Time: 1 ms (98.87%) | Memory: 44.7 MB (74.25%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -5,29 +5,29 @@ class Solution {
 
         int i=0;
         while(i<inter.length){
-            if(merged==false && newIn[1]<inter[i][0]){
+            if(merged==false && newIn[1]<inter[i][0]){ // new interval occurs completely before start of this interval
                 al.add(newIn);
                 merged=true;
             }
-            if(merged==false && newIn[0]<inter[i][0] && newIn[1]>= inter[i][0]){
+            if(merged==false && newIn[0]<inter[i][0] && newIn[1]>= inter[i][0]){ // new interval starts before this interval but overlaps with it
                 inter[i][0] = newIn[0];
                 inter[i][1] = inter[i][1]>newIn[1] ? inter[i][1] : newIn[1];
 
             }
 
-            if(newIn[0] >= inter[i][0] && newIn[0]<= inter[i][1] && merged==false){
+            if(newIn[0] >= inter[i][0] && newIn[0]<= inter[i][1] && merged==false){ // new interval starts somewhere between this interval
                 inter[i][1] = inter[i][1]>newIn[1] ? inter[i][1] : newIn[1];
 
                 merged =  true;
             }
  
-            int[]temp = inter[i];
+            int[]temp = inter[i]; 
 
             while(i+1<inter.length && inter[i+1][0] <= temp[1]){
                 
-                temp[1] = temp[1] < inter[i+1][1] ? inter[i+1][1] : temp[1];
+                temp[1] = temp[1] < inter[i+1][1] ? inter[i+1][1] : temp[1]; // next interval starts somewhere between this interval
                 
-                if(newIn[0] >= temp[0] && newIn[0]<= temp[0] && merged==false){
+                if(newIn[0] >= temp[0] && newIn[0]<= temp[0] && merged==false){ // new interval starts somewhere between this interval
                     temp[1] =  temp[1]>newIn[1] ? temp[1] : newIn[1] ;
                     merged = true;
                 }
@@ -40,7 +40,7 @@ class Solution {
             i++;
         }
 
-        if(merged==false)al.add(newIn);
+        if(merged==false)al.add(newIn); // in case array size is zero or new interval is to be added seperately in last
 
         int[][]ans = new int[al.size()][2];
         i=0;
@@ -49,7 +49,6 @@ class Solution {
             ans[i][0] = e[0];
             ans[i][1] = e[1];
             i++;
-            // System.out.println(e[0]+" "+e[1]);
         }
         return ans;
     }
