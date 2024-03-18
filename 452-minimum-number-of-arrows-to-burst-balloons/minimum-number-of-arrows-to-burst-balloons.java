2b1bc8ae79badaf0bfa018628f54fc63dea3af4a
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-03-18 17:38:31 +0530

    Time: 38 ms (99.09%) | Memory: 75 MB (16.02%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a,b)-> Integer.compare(a[0], b[0]));
+        // Arrays.sort(points, (a,b)-> Integer.compare(a[0], b[0]));
+        quickSort(points, 0, points.length-1);
 
         // for(int[]i : points) System.out.println(i[0] +" " + i[1]);
 
@@ -28,5 +29,32 @@ class Solution {
         }
 
         return arrows;
+    }
+
+     private void quickSort(int[][] arr, int low, int high) {
+        if (low >= high) return;
+
+        random(arr, low, high);
+
+        int i = (low - 1);
+        for (int j = low; j <= high; j++) if (arr[j][1] < arr[high][1]) {
+            swap(arr, j, ++i);
+        }
+        swap(arr, high, ++i);
+
+        quickSort(arr, low, i - 1);
+        quickSort(arr, i + 1, high);
+    }
+
+    private void random(int[][] arr, int low, int high) {
+        int mid = low + (high - low) / 2;
+        swap(arr, mid, high);
+    }
+
+    private void swap(int[][] arr, int x, int y) {
+        if (x == y) return;
+        int[] temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
