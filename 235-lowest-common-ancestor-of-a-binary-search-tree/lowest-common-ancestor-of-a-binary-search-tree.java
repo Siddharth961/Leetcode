9d7fd7c1966e63b432db2dab7bfe10071b18cae5
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-03-22 00:48:06 +0530

    Time: 5 ms (100.00%) | Memory: 45.3 MB (17.33%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -14,10 +14,10 @@ class Solution {
     }
 
     public TreeNode get_ans(TreeNode root, TreeNode p, TreeNode q){
-        if(root == p || root == q) return root;
+        if(root == p || root == q) return root; // one of them is root so next is below it hence ans is root
 
-        if(p.val<root.val && q.val<root.val) return get_ans(root.left, p, q);
-        else if(p.val>root.val && q.val>root.val) return get_ans(root.right, p, q);
-        else return root;
+        if(p.val<root.val && q.val<root.val) return get_ans(root.left, p, q); // both must be in left subtree
+        else if(p.val>root.val && q.val>root.val) return get_ans(root.right, p, q); //both must be in right subtree
+        else return root; // they got seperated here so root is Last common ancestor
     }
 }
