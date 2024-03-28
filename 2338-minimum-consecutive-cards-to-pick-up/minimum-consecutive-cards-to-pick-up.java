000a3e71750d3c29b21576286bde63b6d745cad0
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-03-28 20:11:56 +0530

    Time: 43 ms (74.47%) | Memory: 62.9 MB (17.50%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.java
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.java
@@ -6,8 +6,7 @@ class Solution {
         for(int i =0; i<cards.length; i++){
             if(mp.containsKey(cards[i])){
                 ans = Math.min(ans, i - mp.get(cards[i]) + 1);
-            }          
-            
+            }     
             mp.put(cards[i],i);
         }
 
