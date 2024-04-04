cd99cf5982e1f8316484330267f0c23894dad2ec
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-04-04 17:53:49 +0530

    Time: 1 ms (99.12%) | Memory: 45.3 MB (27.91%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,27 +1,31 @@
 class Solution {
     public int jump(int[] nums) {
-        int[]dp = new int[nums.length];
-        int min = Integer.MAX_VALUE;
+        int n = nums.length;
 
-        for(int i=nums.length-2; i>=0; i--){
-            if(i+nums[i]>=nums.length-1) dp[i] = 1;
-            else if(nums[i]==0) dp[i]=-1;
-            else{
+        int i = 0;
+        int jumps = 0;
 
-                min = Integer.MAX_VALUE;
-                for(int j=i+1; j<nums.length && j<=i+nums[i]; j++){
-                    if(dp[j]==-1) continue;
-                    min = Math.min(min, dp[j]);
+        while(i<nums.length){
+            if(i>=n-1) return jumps;
+
+            if(i+nums[i]>=nums.length-1) return jumps+1;
+
+            int max = 0;
+            int temp = 0;
+            for(int j=1; j<=nums[i]; j++){
+
+                int jump2 = j + nums[i+j];
+                
+                if(max<jump2){
+                    max = jump2;
+                    temp = j;
                 }
-
-                if(min==Integer.MAX_VALUE) dp[i] = -1;
-                else dp[i] = min+1;
-
             }
+            
+            i += temp;
+            jumps++;
         }
 
-        // for(int i:dp) System.out.print(i+" ");
-
-        return dp[0];
+        return jumps;
     }
 }
