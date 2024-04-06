6e216a2ef51b8ce8ae7cf918fe4beb8ceb56a63d
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-04-06 18:43:05 +0530

    Time: 9 ms (96.03%) | Memory: 45.3 MB (70.43%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,36 +1,37 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
-        int open = 0;
+        
+        char[]arr = s.toCharArray();
+        int open=0;
 
-        for(char c : s.toCharArray()){
+        char c = ' ';
+
+        for(int i=0; i<arr.length; i++){
+            c = arr[i];
             if(c=='(') open++;
             else if(c==')'){
-                if(open==0) continue;
+                if(open==0) arr[i] = '*';
                 else open--;
             }
-
-            st.push(c);
         }
-
-
-        StringBuilder sb  =new StringBuilder();
 
         int close = 0;
 
-        while(st.size()>0){
-
-            char c = st.pop();
-
+        for(int i=arr.length-1; i>=0; i--){
+            c = arr[i];
             if(c==')') close++;
             else if(c=='('){
-                if(close==0) continue;
+                if(close==0) arr[i] = '*';
                 else close--;
             }
-
-            sb.append(c);
-
         }
-        return sb.reverse().toString();
+
+        StringBuilder sb = new StringBuilder();
+
+        for( var e : arr){
+            if(e!='*') sb.append(e);
+        }
+
+        return sb.toString();
     }
 }
