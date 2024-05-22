3546c824898051377d5d2ff3ddbcf5e44628728e
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-05-22 19:13:03 +0530

    Time: 11 ms (20.47%) | Memory: 56 MB (87.94%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    List<List<String>> ans = new ArrayList();
+    List<String> al = new ArrayList();
+
+    public List<List<String>> partition(String s) {
+        get_ans("", s);
+        return ans;
+    }
+
+    public void get_ans( String left, String right){
+
+        
+
+        if( check(left)==false ) return;
+
+        if(right.length()==0){
+
+            al.add(left);
+
+            // System.out.println("base case-- " + al );
+            ans.add( new ArrayList<>(al) );
+
+            al.remove(al.size()-1);
+            return;
+        }
+
+        if(left.length()>0) al.add(left);
+
+        for(int i=0; i<right.length(); i++){
+
+            String l = right.substring(0, i+1);
+            String r = right.substring(i+1, right.length());
+            // System.out.println(l+"----"+r);
+            get_ans(l, r);
+
+        }
+
+        if(al.size()>0) al.remove(al.size()-1);
+    }
+
+    public boolean check(String s){
+        if(s.length()==0) return true;
+
+        char[]arr =s.toCharArray();
+
+        int i=0;
+        int j=arr.length-1;
+
+        while(i<=j){
+            if(arr[i] != arr[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+
+    }
+}
