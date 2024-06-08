c11e3e289468bb64612ba935ac585322cd4ffa58
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-06-08 22:35:07 +0530

    Time: 33 ms (5.75%) | Memory: 60.7 MB (28.51%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -4,15 +4,14 @@ class Solution {
         HashMap<Integer, Integer> mp = new HashMap<>();
         mp.put(0, -1);
 
-        int[]prefix = new int[nums.length];
+        int sum=0;
         
 
         for(int i=0; i<nums.length; i++){
             
-            if(i==0) prefix[i] = nums[i];
-            else prefix[i] = (prefix[i-1] + nums[i]);
+            sum += nums[i];
 
-            int val = prefix[i] % k;
+            int val = sum % k;
 
             if(mp.containsKey(val) && i-mp.get(val) > 1) return true;
             if(!mp.containsKey(val)) mp.put(val, i);
