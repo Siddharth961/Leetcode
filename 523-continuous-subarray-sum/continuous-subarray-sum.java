26d829481a7779271cfc083600bfd3f38017b25c
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-06-08 22:35:26 +0530

    Time: 25 ms (34.97%) | Memory: 60.3 MB (33.95%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -5,13 +5,14 @@ class Solution {
         mp.put(0, -1);
 
         int sum=0;
+        int val = 0;
         
 
         for(int i=0; i<nums.length; i++){
             
             sum += nums[i];
 
-            int val = sum % k;
+             val = sum % k;
 
             if(mp.containsKey(val) && i-mp.get(val) > 1) return true;
             if(!mp.containsKey(val)) mp.put(val, i);
