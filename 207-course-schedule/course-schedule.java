01027b87636a7aa1cabffc6ec2b607f38fefe859
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-07-04 01:48:20 +0530

    Time: 5 ms (67.75%) | Memory: 44.8 MB (49.39%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -2,49 +2,46 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         
         ArrayList<Integer>[]graph = new ArrayList[numCourses];
+        int[]inward = new int[numCourses];
 
         for(int i=0; i<graph.length; i++) graph[i] = new ArrayList<>();
+
 
         for(int[]arr : prerequisites){
 
             graph[ arr[1] ].add(arr[0]);
+
+            inward[ arr[0] ]++;
         }
 
 
         boolean[]visited = new boolean[numCourses];
 
-        boolean[]finished = new boolean[numCourses];
+        ArrayList<Integer> topo = new ArrayList<>();
 
-        for(int i=0; i<finished.length; i++){
+        Queue<Integer> q = new LinkedList<>();
 
-           if(finished[i]==false) get_ans(graph, i, visited, finished) ;
+        for(int i=0; i<inward.length; i++) if(inward[i]==0) q.add(i);
+
+        while(q.size()>0){
+
+            int node = q.poll();
+
+            topo.add(node);
+            visited[node] = true;
+
+            for(int n : graph[node] ){
+                
+                if(!visited[n]){
+                    inward[n]--;
+                    if(inward[n]==0)q.add(n);
+                }
+            }
         }
 
-        for( boolean val : finished) if(!val) return false;       
-
-        return true;
+        if(topo.size()==numCourses) return true;
+        return false; 
 
     }
 
-    public void get_ans(ArrayList<Integer>[]graph, int src,boolean[]visited, boolean[]finished){
-
-        if( visited[src]==true ) return ;
-
-        
-
-        visited[src] = true;
-
-        boolean ans = true;
-        
-        for(Integer e : graph[src]){
-            
-            get_ans(graph, e, visited, finished);              
-            
-
-            ans = finished[e];
-            if(ans==false) break;
-        }
-
-        finished[src] = ans;
-    }
 }
