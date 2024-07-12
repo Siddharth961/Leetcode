b15299a8e36d1b66f213df6cf8388ed7e00f6606
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-07-13 01:52:20 +0530

    Time: 103 ms (63.81%) | Memory: 46.7 MB (40.95%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -0,0 +1,78 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        Stack<Character> st = new Stack<>();
+        Stack<Character> lower = new Stack<>();
+        int ans = 0;
+
+        s = s + 'x';
+
+        for(char c : s.toCharArray()){
+
+            if( y > x){
+
+                if(c == 'a' ){
+
+                    if(st.size()>0 && st.peek()=='b'){
+
+                        st.pop();
+                        ans += y;
+                        // System.out.println(ans+"--");
+                    }
+                    else st.push(c);
+                }
+
+                else if( c == 'b') st.push(c);
+
+                else{
+                    lower.clear();
+                    // System.out.println(ans+"--"+st);
+                    while(st.size()>0){
+                        char top = st.pop();
+                        if(top=='b') lower.push(top);
+                        else if(top=='a'){
+                            if(lower.size()>0 && lower.peek()=='b'){
+                                ans += x;
+                                lower.pop();
+                            }
+                            else lower.push(top);
+                        } 
+                    }
+                }
+            }
+
+            else{
+                if(c == 'b' ){
+
+                    if(st.size()>0 && st.peek()=='a'){
+
+                        st.pop();
+                        ans += x;
+                    }
+                    else st.push(c);
+                }
+
+                else if( c == 'a') st.push(c);
+
+                else{
+                    lower.clear();
+                    // System.out.println(ans+"--"+st);
+                    while(st.size()>0){
+                        char top = st.pop();
+                        if(top=='a') lower.push(top);
+                        else if(top=='b'){
+                            if(lower.size()>0 && lower.peek()=='a'){
+                                ans += y;
+                                lower.pop();
+                            }
+                            else lower.push(top);
+                        } 
+                    }
+                }
+            }
+
+        }
+
+        
+        return ans;
+    }
+}
