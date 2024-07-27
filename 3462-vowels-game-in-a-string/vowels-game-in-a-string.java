4af795dcd7db2bfce3d4c7fac751be4bfc9afc75
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-07-27 18:12:30 +0530

    Time: 4 ms (92.12%) | Memory: 45.1 MB (99.14%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,50 +1,15 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        return turn('a', 0, s.toCharArray());
+
+        for(char c : s.toCharArray()){
+            if( check_vowel(c)) return true;
+        }
+
+        return false;
+        
     }
 
-    public boolean turn( char player, int idx, char[]arr ){
-
-        
-
-        if(idx == arr.length){
-            if(player == 'a') return false;
-            return true;
-        }
-
-        int even = -1;
-        int odd = -1;
-
-        int vowels = 0;
-
-        for( int i=idx; i<arr.length; i++){
-            if( check_vowel(arr[i]) ){
-                vowels++;
-                if(vowels%2 == 0) even = i;
-                else odd = i;
-            }
-        }
-
-        
-
-        if(vowels == 0){
-            return player=='a' ? false : true;
-        }
-
-        if(player=='a'){
-            if( even < odd ) idx = arr.length;
-            else idx = even;
-        }
-        else{
-            
-            if( odd < even ) idx = arr.length;
-            else idx = odd;
-        }
-
-        player = (player=='a') ? 'b' : 'a' ;
-
-        return turn(player, idx, arr);
-    }
+    
 
     public boolean check_vowel(char a){
         if( a=='a' || a=='e' || a=='i' || a=='o' || a=='u') return true;
