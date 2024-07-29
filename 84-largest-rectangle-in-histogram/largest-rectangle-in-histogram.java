27cb803c0f4811569e6bc90bb8817941510d4a68
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-07-29 18:31:12 +0530

    Time: 111 ms (31.28%) | Memory: 57.3 MB (62.02%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,47 +1,48 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> st = new Stack<>();
 
         int n = heights.length;
-        int[]right = new int[n];
+        
+        Stack<Integer>st = new Stack<>();
+
         int[]left = new int[n];
-        int[]area = new int[n];
+        int[]right = new int[n];
 
-        //filling array for smallest element on right
+        for(int i=0; i<n; i++){
 
-        for(int i = n-1; i>=0; i--){
-            while(st.size()!=0 && heights[st.peek()]>=heights[i]) st.pop();
+            while(st.size()>0 && heights[st.peek()] >= heights[i] ){
+                st.pop();
+            }
 
-            if(st.size()==0) right[i] = n;
-            else right[i] = st.peek();
-
+            if(st.size()==0) left[i] = -1;
+            else left[i] = st.peek();
             st.push(i);
         }
 
         st.clear();
 
+        for(int i=n-1; i>=0; i--){
 
-        //filling array for smallest element on left
+            while(st.size()>0 && heights[st.peek()] >= heights[i] ){
+                st.pop();
+            }
 
-        for(int i = 0; i<n; i++){
-            while(st.size()!=0 && heights[st.peek()]>=heights[i]) st.pop();
-
-            if(st.size()==0) left[i] = -1;
-            else left[i] = st.peek();
-
+            if(st.size()==0) right[i] = n;
+            else right[i] = st.peek();
             st.push(i);
         }
 
-        
+        // for(int i: left) System.out.print(i+" ");
+        // System.out.println();
+        // for(int i: right) System.out.print(i+" ");
 
-       
-        // filling area array and find max area;
-        int max=-1;
+        int ans = -1;
+
         for(int i=0; i<n; i++){
-            area[i] = heights[i]*(right[i]-left[i]-1);
-            if(area[i]>max) max = area[i];
+            ans  = Math.max(ans, heights[i]*(right[i]-left[i]-1) );
+
         }
 
-        return max;
+        return ans;
     }
 }
