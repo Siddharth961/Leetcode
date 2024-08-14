a58da7b92258401b72d9d496b61b7f86fb711c56
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-08-15 02:51:19 +0530

    Time: 8 ms (80.23%) | Memory: 44.3 MB (68.30%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,28 +1,51 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
 
-        int min = Integer.MAX_VALUE;
-        int max = -1;
+        Arrays.sort(nums);
 
-        for(int i : nums){
-            if(min > i) min = i;
-            if(max < i) max = i;
-        }
+        int hi = nums[nums.length-1] - nums[0];
+        int lo = 0;
 
-        int[]freq = new int[ max-min + 1];
-        int val=0;
+        int mid = 0;
 
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                val = Math.abs(nums[i]-nums[j]);
-                freq[val]++;
+        int val = 0;
+        int candi = 0;
+
+        while(lo<=hi){
+            mid = (lo+hi)/2;
+            val = check(mid, nums);
+
+            // if( val == k ) return mid;
+            // else if(val > k) hi = mid-1;
+            // else lo = mid+1;
+
+            if(val < k) lo = mid+1;
+            else{
+                candi = mid;
+                hi = mid-1;
             }
         }
+        return candi;
+    }
 
-        for(int i=0; i<freq.length; i++){
-            k -= freq[i];
-            if(k <= 0 ) return i;
+    public int check(int tar, int[]nums){
+        int i=0;
+        int j=1;
+
+        int ans = 0;
+        int val = 0;
+
+        while( j < nums.length){
+
+            while(nums[j] - nums[i] > tar){
+                i++;
+                
+            }
+            ans += j-i;
+
+            j++;           
         }
-        return -1;
+
+        return ans;
     }
 }
