b7550fa42950c6f8b1843c2a78589c147f0f692c
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-08-18 04:12:21 +0530

    Time: 6 ms (81.56%) | Memory: 70 MB (77.65%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.java
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.java
@@ -1,40 +1,42 @@
 class Solution {
     public long maxPoints(int[][] points) {
-
+        
         int n = points.length;
         int m = points[0].length;
-        long val = 0;
-        
-        long[][]dp = new long[n][m];
+
+        long[]prev = new long[m];
+        long[]left = new long[m];
+        long[]right = new long[m];
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(i==0){
-                    dp[i][j] = points[i][j];
+            if(i==0){
+                for(int j=0; j<m; j++){
+                    prev[j] = points[i][j];
                 }
-
-                else{
-
-                    val =  0;
-                    for(int k=0; k<m; k++){
-                        val = Math.max( val, dp[i-1][k] - Math.abs(j-k));
-                    }
-
-                    dp[i][j] = points[i][j] + val;
-                }
-
-                // System.out.print(dp[i][j] + " ");
             }
 
-            // System.out.println();
+            else{
+
+                left[0] = prev[0];
+                for(int j=1; j<m; j++){
+                    left[j] = Math.max(left[j-1]-1, prev[j]);
+                    
+                }
+
+                right[m-1] = prev[m-1];
+                for(int j=m-2; j>=0; j--){
+                    right[j] = Math.max( right[j+1]-1, prev[j]);
+                }
+
+                for(int j=0; j<m; j++){
+                    prev[j] = points[i][j] + Math.max(left[j], right[j]);
+                }
+            }
         }
 
         long max = -1;
 
-        for(int j=0; j<m; j++){
-            max = Math.max(max, dp[n-1][j]);
-        }
-
+        for(int j=0; j<m; j++) max = Math.max(max, prev[j]);
         return max;
     }
 }
