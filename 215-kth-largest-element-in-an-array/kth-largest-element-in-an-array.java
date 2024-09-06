9ef373765b91c4e47956d9063f2f1a39bb144bfb
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-09-06 20:49:10 +0530

    Time: 435 ms (6.26%) | Memory: 56.3 MB (97.27%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,93 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Heap pq = new Heap(k);
+        for(int i : nums){
+            if( pq.size() < k) pq.add(i);
+            else{
+                if(pq.peek() < i){
+                    pq.remove();
+                    pq.add(i);
+                }
+            }
+        }
+
+        return pq.peek();
+    }
+
+    public static class Heap{
+        int[]arr;
+        int head = 0;
+        int tail = -1;
+
+        Heap(int size){
+            arr = new int[size];
+        }
+
+        public void add(int val){
+            if(head == 0 && tail == -1){
+                arr[0] = val;
+                head = 0;
+                tail = 0;
+                return ;
+            }
+
+            tail++;
+            arr[tail] = val;
+
+            int ptr = tail;
+            while(ptr != 0){
+                if( arr[ (ptr-1)/2 ] > arr[ptr]){
+                    arr[ptr] = arr[ (ptr-1)/2 ] ;
+                    arr[ (ptr-1)/2 ]  = val;
+                    
+
+                    ptr = (ptr-1)/2;
+                }
+                else break; 
+            }
+
+
+        }
+
+        public int remove(){
+            int val = arr[head];
+            arr[head] = arr[tail];
+            tail--;
+
+            int ptr = head;
+            int child = 2*(ptr+1) - 1;
+
+            int smaller = 0;
+
+            while(ptr<=tail && child<=tail){
+                if(child+1 > tail){
+                    smaller = child;
+                }
+                else{
+                    smaller = arr[child] <= arr[child+1] ? child : child+1;
+                }
+
+                if( arr[ptr] <= arr[smaller] ) break;
+
+                int temp = arr[smaller];
+                arr[smaller] = arr[ptr];
+                arr[ptr] = temp;
+
+                ptr = smaller;
+                child = 2*(ptr+1) - 1;
+            }
+
+            return val;
+        }
+
+        public int peek(){
+            System.out.println(head);
+            return arr[head];
+        }
+
+        public int size(){
+            return tail - head  + 1;
+        }
+
+    }
+}
