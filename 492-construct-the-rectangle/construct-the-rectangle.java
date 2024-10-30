0dd9b3a95cb13837f16b1f68149c29d815e9f977
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-10-30 20:29:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (11.67%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.java
+++ 492-construct-the-rectangle/construct-the-rectangle.java
@@ -6,28 +6,36 @@ class Solution {
         int[]ans = new int[2];
         int min = area;
 
-        int n = (int)Math.sqrt(area) + 1;
+        // int n = (int)Math.sqrt(area) + 1;
 
-        for(int i=n; i>1; i--){
+        // for(int i=n; i>1; i--){
             
-            if( area % i == 0){
-                len = Math.max(i, area/i);
-                wid = Math.min(i, area/i);
+        //     if( area % i == 0){
+        //         len = Math.max(i, area/i);
+        //         wid = Math.min(i, area/i);
 
-                // System.out.println(len+"--"+wid);
+        //         // System.out.println(len+"--"+wid);
 
-                if(min > len - wid){
-                    ans[0] = len;
-                    ans[1] = wid;
-                    min = len-wid;
-                }
-            }
+        //         if(min > len - wid){
+        //             ans[0] = len;
+        //             ans[1] = wid;
+        //             min = len-wid;
+        //         }
+        //     }
+        // }
+
+        // if(len == -1){
+        //     ans[0] = area;
+        //     ans[1] = 1;
+        // }
+
+        int n = (int)Math.sqrt(area) ;
+        while(area % n != 0){
+            n--;
         }
 
-        if(len == -1){
-            ans[0] = area;
-            ans[1] = 1;
-        }
+        ans[0] = area/n;
+        ans[1] = n;
 
         return ans;
     }
