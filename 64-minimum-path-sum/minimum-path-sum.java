991954e0976142ae6c3091174081304d0a3e6ab1
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-11-15 10:42:23 +0530

    Time: 1 ms (99.77%) | Memory: 47.8 MB (23.59%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -5,29 +5,49 @@ class Solution {
         int[][]dp = new int[m][n];
         int val = Integer.MAX_VALUE;
 
-        for(int i=m-1; i>=0; i--){
+        // for(int i=m-1; i>=0; i--){
 
-            for(int j=n-1; j>=0; j--){
+        //     for(int j=n-1; j>=0; j--){
 
-                if(i==m-1 && j == n-1){
-                    dp[i][j] = grid[i][j];
-                    continue;
-                }
+        //         if(i==m-1 && j == n-1){
+        //             dp[i][j] = grid[i][j];
+        //             continue;
+        //         }
                 
                 
-                val = Integer.MAX_VALUE;
-                if(i+1<m){
-                    val = Math.min(val, dp[i+1][j]);
-                }
-                if(j+1 < n){
-                    val = Math.min(val, dp[i][j+1]);
-                }
+        //         val = Integer.MAX_VALUE;
+        //         if(i+1<m){
+        //             val = Math.min(val, dp[i+1][j]);
+        //         }
+        //         if(j+1 < n){
+        //             val = Math.min(val, dp[i][j+1]);
+        //         }
 
-                dp[i][j] = val + grid[i][j];
+        //         dp[i][j] = val + grid[i][j];
         
-            }
-        }
+        //     }
+        // }
 
-        return dp[0][0];
+        // return dp[0][0];
+
+        return get_ans(0, 0, grid, dp);
+    }
+
+    public int get_ans(int i, int j, int[][]grid, int[][]dp){
+        if(i==grid.length || j==grid[0].length) return Integer.MAX_VALUE;
+
+        if(dp[i][j]!=0) return dp[i][j];
+
+        int a = get_ans(i+1, j, grid, dp);
+        int b = get_ans(i, j+1, grid, dp);
+        
+        int ans = Math.min(a,b) ;
+        if(ans == Integer.MAX_VALUE) ans = 0 ;
+
+        ans += grid[i][j];
+        
+        dp[i][j] = ans;
+        return ans;
+
     }
 }
