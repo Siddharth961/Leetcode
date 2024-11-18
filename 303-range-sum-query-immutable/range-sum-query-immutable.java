9f2a81b5db3b3ed55d50ffeb158225a2bedbc0a7
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-11-18 17:04:52 +0530

    Time: 10 ms (34.41%) | Memory: 49.3 MB (47.35%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,26 +1,52 @@
 class NumArray {
-    int[]arr;
-        int[] num;
 
+    int[]tree;
 
     public NumArray(int[] nums) {
-        arr = new int[nums.length];
-        int prefix=0;
-        for(int i=0; i<nums.length; i++){
-            prefix += nums[i];
-            arr[i] = prefix;
-            
-        }
-
-        num = nums;
-
+        tree = new int[4*nums.length];
+        make_tree(0, 0, nums.length-1, tree, nums);
+        
     }
     
     public int sumRange(int left, int right) {
+        return search(0, 0, tree.length/4 - 1, left, right, tree);
+    }
+
+    public int search( int curr, int left, int right, int tar_left, int tar_right,int[]tree ){
+
+        if(tar_right < left || right < tar_left) return 0;
+
+        if(left >= tar_left && right<= tar_right ){
+            return tree[curr];
+        }
+
+        int mid = (left+right)/2;
+        int ans = 0;
+
         
-        return  arr[right] - arr[left] + num[left];
+
+        return search(2*curr+1, left, mid, tar_left, tar_right, tree) + search(2*curr+2, mid+1, right, tar_left, tar_right, tree);
+
+
+    }
+
+    
+
+    public void make_tree(int curr, int left, int right, int[]arr, int[]nums){
+        if(left == right){
+            arr[curr] = nums[left];
+            return ;
+        }
+
+        int mid = (left+right)/2;
+        make_tree(2*curr + 1, left, mid, arr, nums);
+        make_tree(2*curr + 2, mid+1, right, arr, nums);
+
+        arr[curr] = arr[2*curr + 1] + arr[2*curr+2];
+
     }
 }
+
 
 /**
  * Your NumArray object will be instantiated and called as such:
