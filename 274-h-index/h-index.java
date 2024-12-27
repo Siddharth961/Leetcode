b6588d16ab2e6ea97fb681ddd5db1d934da836bd
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2024-12-27 16:45:46 +0530

    Time: 11 ms (6.15%) | Memory: 41.9 MB (15.17%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,17 +1,21 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int[]check = new int[1001];
+        Arrays.sort(citations);
+        int ans = 0;
+        int count = 1;
 
-        for(int i : citations) check[i]++;
-        int count = 0;
+        for(int i=citations.length - 1; i>=0; i--){
 
-        for(int i = 1000; i>=0; i--){
-            if( check[i] > 0 ){
-                count += check[i];
+            if( citations[i] >= count){
+                ans = Math.min(count, citations[i]);
+                System.out.println(ans);
+                count++;
             }
-                if(count >= i ) return i;
+            else return ans;
         }
 
-        return 0;
+        return ans;
+
+
     }
 }
