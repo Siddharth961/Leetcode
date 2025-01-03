8fc75acaba41e1c0e4de3be6c0940d71b2b7676e
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-01-04 02:49:59 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (43.78%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,73 +1,71 @@
 class Solution {
 
     public class Point{
-        int i;
-        int j;
-
-        Point(int i, int j){
-            this.i = i;
-            this.j = j;
+        int x;
+        int y;
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
         }
     }
+
     public int orangesRotting(int[][] grid) {
 
-        int m = grid.length;
-        int n = grid[0].length;
-
         Queue<Point> q = new LinkedList<>();
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
 
-                if(grid[i][j]==2) q.add( new Point(i, j)); 
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == 2) q.add( new Point(i, j));
             }
         }
 
-        q.add(new Point(-1, -1)); // level end inidicator
-
+        q.add( new Point(-1, -1));
         int ans = 0;
+        int n = grid.length;
+        int m = grid[0].length;
 
         while(q.size() > 1){
 
             Point p = q.poll();
 
-            if(p.i==-1){
+            if(p.x == -1){
                 q.add(p);
                 ans++;
                 continue;
             }
 
-            int i = p.i;
-            int j = p.j;
+            int i = p.x;
+            int j = p.y;
 
-            if( i-1>=0 && grid[i-1][j]==1){
+            if(i-1 >=0 && grid[i-1][j] == 1){
                 grid[i-1][j] = 2;
-                q.add(new Point(i-1, j));
+                q.add( new Point(i-1, j));
             }
-            if( j+1<n && grid[i][j+1]==1){
-                grid[i][j+1] = 2;                
-                q.add(new Point(i, j+1));
+
+            if(j+1 < m && grid[i][j+1] == 1){
+                grid[i][j+1] = 2;
+                q.add( new Point(i, j+1));
             }
-            if( i+1<m && grid[i+1][j]==1){
-                grid[i+1][j] = 2;
-                q.add(new Point(i+1, j));
+            if(i+1 <n && grid[i+1][j] == 1){
+                grid[i+1][j] = 2; 
+                q.add( new Point(i+1, j));
             }
-            if( j-1>=0 && grid[i][j-1]==1){
+            if(j-1 >=0 && grid[i][j-1] == 1){
                 grid[i][j-1] = 2;
-                q.add(new Point(i, j-1));
+                q.add( new Point(i, j-1));
             }
+
+
 
         }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-
-                if(grid[i][j]==1) return -1; 
-            }
+        for(int[]arr : grid){
+            for(int i : arr) if( i == 1) return -1;
         }
 
-        return ans; 
+        return ans;
 
 
+        
     }
 }
