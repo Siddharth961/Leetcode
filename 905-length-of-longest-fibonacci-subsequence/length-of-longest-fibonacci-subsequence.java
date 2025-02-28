8e516648e5bc0f86caca0fc2e48fda388f0dc450
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-02-28 17:39:23 +0530

    Time: 252 ms (13.33%) | Memory: 57.6 MB (5.22%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,35 +1,40 @@
 class Solution {
     public int lenLongestFibSubseq(int[] arr) {
-        int ans =  get_ans(0, 0, 0, 0, arr);
 
-        return ans <= 2 ? 0 : ans;
-    }
+        int max = 0;
+        int n=  arr.length;
+        int[][]dp = new int[n][n];
 
-    public int get_ans(int idx, int a, int b, int len, int[]arr){
-        if(idx == arr.length){
-            return len;
-        }
-            // System.out.println("idx: "+idx+" a: " +a+" b: " + b + " len: " + len);
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        for(int i=0; i<n; i++){
 
-        int ans = len;
+            mp.put(arr[i], i);
 
-        for(int i=idx; i<arr.length; i++){
+            for(int j=i+1; j<n; j++){
 
-            if(len < 2){
-                // a = b;
-                // b = arr[i];
-                ans = Math.max(ans, get_ans(i+1, b, arr[i], len+1, arr) ); 
-            }
-            else{
-                if(arr[i] == a+b ){
-                    // a = b;
-                    // b = arr[i];
-                    ans = Math.max(ans, get_ans(i+1, b, arr[i], len+1, arr) );
+                int diff = arr[j] - arr[i];
+
+                int idx = mp.getOrDefault(diff, -1);
+
+                // if(i == 1 && j==2 ) System.out.println(dp[0][1]);
+
+                if( idx >= 0 && idx < i){
+                    dp[i][j] = dp[idx][i] + 1;
                 }
-                else if( arr[i] > a+b ) break;
+                else dp[i][j] = 2;
+
+                max = Math.max(max, dp[i][j]);
+
+                
             }
         }
 
-        return ans;
+        // for(int[]a : dp){
+        //     for(int i: a) System.out.print(i+" ");
+        //     System.out.println();
+        // }
+
+        return max > 2 ? max : 0;
     }
 }
