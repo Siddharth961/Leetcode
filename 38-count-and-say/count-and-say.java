cb5a98975a8e5f16573a3c57f0d075a1f3e56f77
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-04-18 15:50:41 +0530

    Time: 8 ms (38.96%) | Memory: 41.1 MB (94.23%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,39 +1,70 @@
 class Solution {
     public String countAndSay(int n) {
-        return helper(n).toString();
+        // return helper(n).toString();
+
+        StringBuilder prev = new StringBuilder();
+        prev.append('1');
+        StringBuilder curr = new StringBuilder();
+
+        for (int i = 2; i <= n; i++) {
+
+            int count = 1;
+            int j = 1;
+            for (; j < prev.length(); j++) {
+
+                if (prev.charAt(j - 1) != prev.charAt(j)) {
+
+                    curr.append(count);
+                    curr.append(prev.charAt(j - 1));
+                    count = 1;
+                }
+
+                else
+                    count++;
+            }
+
+            curr.append(count);
+            curr.append(prev.charAt(j - 1));
+
+            prev = curr;
+            curr = new StringBuilder();
+
+        }
+
+        return prev.toString();
     }
 
-    public StringBuilder helper(int n){
+    public StringBuilder helper(int n) {
 
-        if(n == 1){
+        if (n == 1) {
             StringBuilder sb = new StringBuilder();
             sb.append('1');
             return sb;
         }
 
-        StringBuilder s = helper(n-1);
+        StringBuilder s = helper(n - 1);
 
         StringBuilder sb = new StringBuilder();
 
         int count = 1;
-        int i=1;
-        for(; i<s.length(); i++){
+        int i = 1;
+        for (; i < s.length(); i++) {
 
-            if( s.charAt(i-1) != s.charAt(i)){
+            if (s.charAt(i - 1) != s.charAt(i)) {
 
                 sb.append(count);
-                sb.append( s.charAt(i-1) );
+                sb.append(s.charAt(i - 1));
                 count = 1;
             }
 
-            else count++;
+            else
+                count++;
         }
 
         sb.append(count);
-        sb.append(s.charAt(i-1));
+        sb.append(s.charAt(i - 1));
 
         return sb;
-
 
     }
 }
