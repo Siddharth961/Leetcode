b2a5bfccb40dcfd16e18be943324c81a93f5b7bf
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-04-19 19:54:50 +0530

    Time: 50 ms (75.84%) | Memory: 58.9 MB (86.06%) - LeetSync

---
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long countGood(int[] nums, int k) {
+        int n = nums.length;
+
+        // Safe subarray count
+        long total_subarray = (long) n * (n + 1) / 2;
+
+        long discard_subarray = atmost(nums, k - 1);
+
+        return total_subarray - discard_subarray;
+    }
+
+    public long atmost(int[] nums, int k) {
+        int i = 0;
+        long ans = 0;
+        long pairs = 0;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for (int j = 0; j < nums.length; j++) {
+            int freq = mp.getOrDefault(nums[j], 0);
+            pairs += freq;
+            mp.put(nums[j], freq + 1);
+
+            while (pairs > k) {
+                int count = mp.get(nums[i]);
+                pairs -= (count - 1);
+                if (count == 1) {
+                    mp.remove(nums[i]);
+                } else {
+                    mp.put(nums[i], count - 1);
+                }
+                i++;
+            }
+
+            ans += j - i + 1;
+        }
+
+        return ans;
+    }
+}
