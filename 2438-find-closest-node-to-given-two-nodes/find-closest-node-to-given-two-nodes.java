2ccfd731512180f053a99d45dfc1f44b6cc8c68d
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-05-14 16:16:31 +0530

    Time: 122 ms (8.14%) | Memory: 71 MB (13.03%) - LeetSync

---
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        
+        HashMap<Integer, Integer> st1 = new HashMap<>();
+        HashMap<Integer, Integer> st2 = new HashMap<>();
+
+        int ptr1 = node1;
+        int ptr2 = node2;
+
+        int dist = 0;
+
+        while(ptr1 != -1 && !st1.containsKey(ptr1) ){
+
+            st1.put(ptr1, dist);
+            ptr1 = edges[ptr1];
+
+            dist++;
+
+        }
+
+        dist = 0;
+        int min_dist = Integer.MAX_VALUE;
+        int ans = -1;
+
+        while(ptr2 != -1 && !st2.containsKey(ptr2)){
+
+            if(st1.containsKey(ptr2)){
+
+                int val = st1.get(ptr2);
+
+                if(min_dist > Math.max(dist , val) ){
+
+                    ans = ptr2;
+                    min_dist = Math.max(dist , val) ;
+                }
+
+                if(min_dist == Math.max(dist , val) ) ans = Math.min(ans, ptr2);
+
+                
+            }
+
+            st2.put(ptr2, dist);
+            ptr2 = edges[ptr2];
+
+            dist++; 
+        }
+
+        System.out.println(st1);
+        System.out.println(st2);
+
+
+
+        return ans ;
+    }
+}
