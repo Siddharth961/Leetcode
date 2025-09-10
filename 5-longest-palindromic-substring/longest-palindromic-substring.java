118b374342e191c780b6ace51d9eced3c584b1a0
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-11 01:40:37 +0530

    Time: 16 ms (61.59%) | Memory: 45.1 MB (44.68%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,50 +1,79 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int max = 0;
-        int idx = 0;
-        int i = 0;
-        char[]arr = s.toCharArray();
+        String ans = "";
+        int f_len = 0;
 
-        int[]result ;
-        while(i < arr.length){
+        int f_i = -1;
+        int f_j = -1;
 
-            result = check(i, i, arr);
+        int[]arr;
+        int len = 0;
 
-            if(max < result[1]){
-                max = result[1];
-                idx = result[0];
+        for(int i=0; i<s.length(); i++){
+
+            // ans = Math.max(ans, getlen(0, i, s) );
+            // ans = Math.max(ans, getlen(1, i, s) );
+
+            arr = getlen(0, i, s);
+
+            len = arr[1] - arr[0] + 1;
+
+            if(len > f_len){
+                f_i = arr[0];
+                f_j = arr[1];
+
+                f_len = len;
             }
 
-            result = check(i, i+1, arr);
 
-            if(max < result[1]){
-                max = result[1];
-                idx = result[0];
+            arr = getlen(1, i, s);
+
+            len = arr[1] - arr[0] + 1;
+
+            if(len > f_len){
+                f_i = arr[0];
+                f_j = arr[1];
+
+                f_len = len;
             }
-
-            i++;
 
         }
 
-        return s.substring(idx, idx+max);
+        return s.substring(f_i, f_j + 1);
     }
 
-    public int[] check(int i, int j, char[]arr){
+    public int[] getlen( int type, int idx, String s){
 
-        
+        // type = 0 -> evenlen
+        // type = 1 -> oddlen
 
-        int[]ans = new int[2];
+        int i = idx-1;
+        int j = type == 0 ? idx : idx+1;
 
-        while( i>=0 && j<arr.length){
-            if(arr[i] == arr[j]){
-                ans[0] = i;
-                ans[1] = j-i+1;
+        int len = type == 0 ? 0 : 1;
+
+        while(i>=0 && j<s.length()){
+
+            if( s.charAt(i) == s.charAt(j)){
                 i--;
                 j++;
+
+                len++;
             }
-            else return ans;
+
+            else break;
         }
 
+        int[]ans = new int[2];
+        ans[0] = i+1;
+        ans[1] = j-1;
+
+        // System.out.println(i + " " + j);
+
         return ans;
+
+
     }
+
+
 }
