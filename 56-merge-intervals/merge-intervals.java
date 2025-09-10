9d443bbc4d81aff709c67413d7982f52f8e75abb
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-11 01:54:53 +0530

    Time: 8 ms (85.19%) | Memory: 46.4 MB (93.84%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,52 +1,36 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort( intervals, (a,b) -> a[0] - b[0]);
 
-        ArrayList<int[]> al = new ArrayList<>();
+        ArrayList<int[]>al = new ArrayList<>();
 
         int i=0;
-        int j=0;
-        int start = 0;
-        int end=0;
+        int j=1;
 
-        while(i < intervals.length){
+        while(j < intervals.length){
 
-            start = intervals[i][0];
-
-            end = intervals[i][1];
-
-            if(i+1 < intervals.length && end>= intervals[i+1][0]){
-
-                while(i+1 < intervals.length && end>= intervals[i+1][0]){
-                    end = Math.max(end, intervals[i+1][1]);
-                    i++;
-                }
-
-                int[] arr = new int[2];
-                arr[0] = start;
-                arr[1] = end;
-                
-                al.add( arr );
+            if( intervals[i][1] >= intervals[j][0] ){
+                intervals[i][1] = Math.max(intervals[i][1], intervals[j][1] );
             }
+
             else{
                 al.add(intervals[i]);
-                
+                i = j;
             }
 
-            i++;
-
+            j++;
         }
+
+        al.add(intervals[i]);
 
         int[][]ans = new int[al.size()][];
 
         int idx = 0;
-
-        for(int[] arr : al){
-            ans[idx] = arr;
+        for(int[]a : al){
+            ans[idx] = a;
             idx++;
         }
-
 
         return ans;
     }
