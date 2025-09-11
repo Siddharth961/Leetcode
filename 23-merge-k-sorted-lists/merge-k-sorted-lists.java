96e52226c3c9803ef1786bad1d2a114b33248dae
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-11 20:35:42 +0530

    Time: 4 ms (78.66%) | Memory: 44.5 MB (65.91%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,41 +9,25 @@
  * }
  */
 class Solution {
-    public class Pair implements Comparable<Pair>{
-        ListNode node;
-        int val;
-
-        Pair(ListNode p){
-            node = p;
-            val = p.val;
-        }
-
-        @Override
-        public int compareTo( Pair p2 ){
-            return this.val - p2.val;
-        }
-
-    }
     public ListNode mergeKLists(ListNode[] lists) {
-
-        if(lists.length==0) return null;
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-
-        for(int i=0; i<lists.length; i++){
-            
-            if(lists[i]!=null) pq.add( new Pair(lists[i]) );
-        }
-
         ListNode chead = new ListNode();
+
         ListNode ptr = chead;
 
-        while(pq.size()>0){
-            Pair p = pq.poll();
-            ptr.next = p.node;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>( (a,b) -> a.val - b.val);
+
+        for(ListNode l : lists) if(l!=null)pq.add(l);
+
+        while(pq.size() > 0){
+            ptr.next = pq.remove();
+
             ptr = ptr.next;
 
-            if( p.node.next != null) pq.add(new Pair( p.node.next));
+            if(ptr.next != null) pq.add(ptr.next);
+
+            ptr.next = null;
+
         }
 
         return chead.next;
