1c36db5728f8ba7bb7d13385e2144a99145246ee
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-13 00:20:38 +0530

    Time: 2 ms (28.77%) | Memory: 45 MB (73.46%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,22 +1,47 @@
 class Solution {
     public int jump(int[] nums) {
         
-        int[]dp = new int[nums.length];
+        // int[]dp = new int[nums.length];
 
 
-        for(int i=nums.length-2; i>=0; i--){
+        // for(int i=nums.length-2; i>=0; i--){
             
-            int val = Integer.MAX_VALUE-1;
-            for(int j=i+1; j<=i+nums[i] && j<nums.length; j++){
+        //     int val = Integer.MAX_VALUE-1;
+        //     for(int j=i+1; j<=i+nums[i] && j<nums.length; j++){
 
-                val = Math.min(val, dp[j] + 1);
+        //         val = Math.min(val, dp[j] + 1);
 
+        //     }
+
+        //     dp[i] = val ;
+
+        // }
+
+        // return dp[0];
+
+        int farthest_point = 0;
+        int next_point = 0;
+
+        int i=0;
+        int jumps = 0;
+
+        while(i < nums.length){
+
+            while(i<nums.length && i<=farthest_point){
+
+                if(i == nums.length - 1) return jumps;
+                next_point = Math.max(next_point, i + nums[i]);
+                i++;
             }
 
-            dp[i] = val ;
+            jumps++;
 
+            if(next_point >= nums.length - 1) return jumps;
+
+            farthest_point = next_point;
+            next_point = 0;
         }
 
-        return dp[0];
+        return jumps;
     }
 }
