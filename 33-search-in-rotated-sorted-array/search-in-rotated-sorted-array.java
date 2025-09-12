d1de002d08f83f4760afffa96f2afa528b67333f
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-12 19:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (97.71%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,43 +1,43 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==1) return nums[0]==target ? 0 : -1;
-        int l = 0;
-        int h = nums.length - 1;
+        
+        int pivot = 0;
+        int l=0;
+        int h=nums.length-1;
+        int mid=0;
+
+        while( l <= h ){
+
+            mid = (l+h)/2;
+
+            if( nums[mid] >= nums[0]){
+                l = mid+1;
+            }
+            else{
+                pivot = mid;
+                h = mid-1;
+            }
+
+        }
+
+        int ans = search(0, pivot-1, nums, target);
+
+        if(ans != -1) return ans;
+
+        return search(pivot, nums.length-1, nums, target);
+
+    }
+
+    public int search(int l, int h, int[]nums, int tar){
+
         int mid = 0;
 
-        int smallest = 0;
-
-        if(nums[l]<nums[h]) smallest = l;
-
-        else{
-            while(l<=h){
+        while(l <= h){
             mid = (l+h)/2;
-            if(mid-1>=0 && nums[mid-1] > nums[mid]){
-                smallest = mid;
-                break;
-            }
-            else if( nums[mid] > nums[h]) l = mid + 1;
-            else h = mid - 1;
-            }
-        }
 
-        if(nums[smallest] == target) return smallest;
+            if(nums[mid] == tar) return mid;
 
-        if(target <= nums[nums.length-1]){
-            l = smallest + 1;
-            h = nums.length-1;
-        }
-        else{
-            l = 0;
-            h = smallest - 1;
-        }
-
-        
-
-        while(l<=h){
-            mid = (l+h)/2;
-            if(nums[mid]==target) return mid;
-            else if( nums[mid] < target) l = mid+1;
+            if(nums[mid] < tar) l = mid+1;
             else h = mid-1;
         }
 
