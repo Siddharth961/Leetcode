10e2c70e7fff408641695c1160fbc34aad5e426a
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-13 16:15:13 +0530

    Time: 158 ms (35.69%) | Memory: 41.6 MB (62.68%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,44 +1,43 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean ans = false;
 
-        boolean[][] backtrack = new boolean[board.length][board[0].length];
-
+        boolean[][]visited = new boolean[board.length][ board[0].length];
+        
         for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                ans = get_ans(i, j, board, 0, word, backtrack);
-                if(ans==true) break;
-            }
-            if(ans==true) break;
+            for(int j = 0; j<board[0].length; j++){
 
+                if( check(i, j, 0, word, board, visited) ) return true;
+            }
         }
 
-        return ans;
+        return false;
     }
 
-    public boolean get_ans(int i, int j, char[][]board, int idx, String word, boolean[][] backtrack){
-        if(idx==word.length()) return true;
-        if(i<0  || i >= board.length || j<0 || j>=board[0].length) return false;
-        if(board[i][j] != word.charAt(idx) || backtrack[i][j]==true) return false;
+    public boolean check(int i, int j, int idx, String word, char[][]board, boolean[][]visited){
+
+        if(idx == word.length() ) return true;
+
+        if(i<0 || j<0 || i==board.length || j==board[0].length){
+            return false;
+        }
+
+        if(board[i][j] != word.charAt(idx) || visited[i][j]) return false;
+
+        visited[i][j] = true;
 
         boolean ans = false;
 
-        backtrack[i][j] = true;
+        ans = ans || check(i-1, j, idx+1, word, board, visited);
+        ans = ans || check(i, j+1, idx+1, word, board, visited);
+        ans = ans || check(i+1, j, idx+1, word, board, visited);
+        ans = ans || check(i, j-1, idx+1, word, board, visited);
 
-        ans = get_ans(i-1, j, board, idx+1, word, backtrack);
-        if(ans==true) return ans;
+        // if(ans == true){
+        //     System.out.println(i + " " + j+ " " + idx);
+        // }
 
-        ans = get_ans(i, j+1, board, idx+1, word, backtrack);
-        if(ans==true) return ans;
-
-        ans = get_ans(i+1, j, board, idx+1, word, backtrack);
-        if(ans==true) return ans;
-
-        ans = get_ans(i, j-1, board, idx+1, word, backtrack);
-
-        backtrack[i][j] = false;
+        visited[i][j] = false;
 
         return ans;
-
     }
 }
