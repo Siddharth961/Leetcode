d96d00ccf49ea63a4161ffe45f8fce148ba254ec
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-13 19:40:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (57.03%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,36 +1,53 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        for(int i=nums.length-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
 
-        int i=0;
-        
-        for( i=nums.length-2; i>=0; i--){
+                int j=i+1;
 
-            if(nums[i]<nums[i+1]){
-                break;
-            }
-        }
-
-        if(i >= 0){
-            int min = 200;
-            int idx = -1;
-
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[j] > nums[i] && nums[j]<min){
-                    min = nums[j];
-                    idx = j;
+                for(; j<nums.length; j++){
+                    if(nums[j] <= nums[i]) break;
                 }
+
+                j--;
+
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                sort(i+1, nums);
+
+                return ;
             }
-
-            nums[idx] = nums[i];
-            nums[i] = min;
-
-
-            Arrays.sort(nums, i+1, nums.length);
         }
 
-        else Arrays.sort(nums);
+        for(int i=0; i<nums.length/2; i++){
 
+            int temp = nums[i];
+            nums[i] = nums[ nums.length - i - 1];
+            nums[ nums.length - i - 1] = temp;
+        }
+    }
 
-        
+    public void sort(int idx, int[]nums){
+
+       int i=idx;
+       int j=nums.length -1;
+
+       while(i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+            i++;
+            j--;
+       }
     }
 }
+
+//  1 3 2 1
+//  2 1 3 1 X
+//  2 1 1 3
+
+//  2 6 5 4 3 0
+//  3 2 
