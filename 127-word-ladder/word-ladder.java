4eb6e24d6cea5a0534617b4c7e05cf7a188a0751
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-17 01:13:09 +0530

    Time: 1593 ms (5.00%) | Memory: 48.3 MB (18.47%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,133 +1,61 @@
 class Solution {
-    public int ladderLength(String begin, String end, List<String> list) {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
-        List< List<Integer> > graph = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
 
-        for(int i=0; i<list.size(); i++){
-            graph.add( new LinkedList<>() );
-        }
+        Queue<String> q = new LinkedList<>();
 
-        int src = -1;
-        int tar = -1;
+        int level = 1;
 
-        String s;
+        q.add(beginWord);
 
-        for(int i=0; i<list.size(); i++){
+        while(q.size() > 0){
 
-            s = list.get(i);
+            // System.out.println(q);
 
-            if(s.equals(begin) ) src = i;
-            if(s.equals(end) ) tar = i;
+            int size = q.size();
 
-            for(int j = 0; j<list.size(); j++){
+            while(size > 0){
 
-                if(i == j) continue;
-                
-                if(compare( i, j, list) ){
-                    graph.get(i).add(j); 
+                String s = q.remove();
+                size--;
+
+                if( s.equals(endWord) ) return level;
+
+                if( visited.contains(s) ) continue;
+                visited.add(s);
+
+                for(String b : wordList){
+
+                    if( !visited.contains(b) && check(s,b) ){
+                         q.add(b);
+                    }
                 }
-            }
-        }
 
-        if(src == -1){
-
-            graph.add( new LinkedList<>() );
-            list.add(begin);
-
-            src = graph.size()-1;
-
-            for(int j = 0; j<list.size(); j++){
-                
-                if(compare( src, j, list) ){
-                    graph.get(src).add(j);
-                    graph.get(j).add(src);
-                }
             }
 
-            
-        }
-
-        // for(int i = 0; i<graph.size(); i++){
-        //     System.out.print(i+" -- ");
-
-        //     for(int j : graph.get(i)){
-        //         System.out.print(j+" ");
-        //     }
-
-        //     System.out.println();
-        // }
-        // System.out.println("------------");
-
-        return travel(src, tar, graph);
-
-    }
-
-    public boolean compare(int i, int j, List<String> list){
-
-        String s1 = list.get(i);
-        String s2 = list.get(j);
-
-        boolean change = false;
-
-        for(int k=0; k<s1.length(); k++){
-
-            if( s1.charAt(k) != s2.charAt(k) ){
-                if(change) return false;
-                else change = true;
-            }
-
-        }
-
-        return change;
-
-    }
-
-    public class Pair implements Comparable<Pair>{
-        int node;
-        int dist;
-
-        Pair(int n, int d){
-            node = n;
-            dist = d;
-        }
-
-        @Override
-        public int compareTo(Pair p2){
-            return this.dist - p2.dist;
-        }
-    }
-
-    public int travel( int src, int tar, List<List<Integer>> graph){
-        
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        boolean[]visited = new boolean[graph.size()];
-
-        int[]distance = new int[graph.size()];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-
-        distance[src] = 1;
-
-        pq.add( new Pair(src, 1));
-
-        while(pq.size()>0 ){
-
-            Pair p = pq.poll();
-
-            if( visited[p.node] ) continue;
-
-            if(p.node == tar) return p.dist;
-
-            visited[p.node] = true;
-
-            for(int neighbour : graph.get(p.node) ){
-
-                if(distance[neighbour] > distance[p.node] + 1){
-                    distance[neighbour] = distance[src] + 1;
-                    pq.add( new Pair(neighbour, p.dist+1 ) );
-                }
-            }
+            level++;
         }
 
         return 0;
     }
+
+    public boolean check (String s, String b){
+        
+        int[]freq = new int[26];
+
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+
+            if(s.charAt(i) != b.charAt(i) ) count++;
+
+        }
+
+
+        
+
+        return count == 1;
+    }
 }
+
+// leet lest lost lose lode code
