e58202c3225b89849b7346b6dd4a780d6e566a79
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-17 01:56:53 +0530

    Time: 6 ms (100.00%) | Memory: 44.8 MB (47.68%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,38 +8,31 @@
  * }
  */
 class Solution {
-    List<TreeNode> al = new ArrayList<>();
+    TreeNode ans = null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        get_path(root, p);
-        List<TreeNode> p_list = al;
-        al = new ArrayList<>();
-        get_path(root, q);
-        List<TreeNode> q_list = al;
 
-        int i = 0;
-        int j = 0;
-        if(p_list.size() > q_list.size()) i = p_list.size() - q_list.size();
-        if(p_list.size() < q_list.size()) j = q_list.size() - p_list.size();
+        travel(root, p, q);        
 
-        while( p_list.get(i) != q_list.get(j)){
-            i++;
-            j++;
-        }
-        return p_list.get(i);
-
+        return ans;
+        
     }
 
-    public boolean get_path( TreeNode root, TreeNode tar){
+    public boolean travel(TreeNode root, TreeNode p, TreeNode q){
+
         if(root == null) return false;
-        if(root == tar){
-            al.add(root);
-            return true;
+
+        boolean left = travel(root.left, p, q);
+        boolean right = travel(root.right, p, q);
+
+        boolean curr = root == p || root == q;
+
+        if( left && right ) ans = root;
+
+        else if( (left || right) && curr){
+            
+            ans = root;
         }
 
-        boolean a = get_path(root.left, tar);
-        if(a==false) a = get_path(root.right, tar);
-
-        if(a==true) al.add(root);
-        return a;
+        return left || right || curr;
     }
 }
