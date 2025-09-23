28c2adeceb7714abf95af9e7c4d88c2dd46a21b2
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-23 23:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (66.35%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,42 +1,50 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0;
-        int j = matrix.length-1;
+        
+        int row = getRow(matrix, target);
+
+        return search(row, matrix, target);
+    }
+
+    public int getRow(int[][] matrix, int target){
+        int l=0;
+        int h = matrix.length-1;
         int mid = 0;
 
-        while(i<=j){
-            mid = (i+j)/2;
+        int ans = 0;
 
-            if(matrix[mid][0] > target) j = mid - 1;
-            else if(matrix[mid][0] < target) i = mid+1;
-            else break;
+        while(l <= h){
+            mid = (l+h)/2;
+
+            if(matrix[mid][0] <= target ){
+                ans = mid;
+                l = mid+1;
+            }
+            else h = mid-1;
         }
 
-        int r = 0;
-        if(matrix[mid][0] == target || (mid==matrix.length-1 && matrix[mid][0]<target)) r = mid;
-        else{
-            if(mid+1<matrix.length && matrix[mid][0]<target){
-                if(matrix[mid+1][0]>target) r = mid;
-                else r = mid+1;
-            }
-            if(mid-1>=0 && matrix[mid][0]>target){
-                 r = mid - 1;
-            }
-        
-        }
+        return ans;
+    }
 
-        System.out.println(mid+" "+r);
+    public boolean search(int row, int[][]matrix, int target){
 
-        i = 0;
-        j = matrix[0].length-1;
-        
-        while(i<=j){
-            mid = (i+j)/2;
-            if(matrix[r][mid] > target) j = mid-1;
-            else if(matrix[r][mid] < target) i = mid+1;
-            else return true;
+        int l = 0;
+        int h = matrix[0].length-1;
+        int mid = 0;
+
+        while(l <= h){
+            mid = (l+h)/2;
+
+            if(matrix[row][mid] == target ){
+                return true;
+            }
+
+            if(matrix[row][mid] <= target) l = mid+1;
+
+            else h = mid-1;
         }
 
         return false;
+
     }
 }
