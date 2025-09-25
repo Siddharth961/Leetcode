3592da3b8d60974a71c704401492a22a3367d9a7
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-26 03:11:42 +0530

    Time: 2 ms (11.29%) | Memory: 45.7 MB (6.13%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new LinkedList<>();
+
+        int i=1;
+        while(i <= n){
+            if( i%3 == 0 && i%5 == 0){
+                ans.add("FizzBuzz");
+
+            }
+            else if(i%3 == 0){
+                ans.add("Fizz");
+
+            }
+            else if(i%5 == 0){
+                ans.add("Buzz");
+
+            }
+            else ans.add(i+"");
+
+            i++;
+        }
+
+        return ans;
+    
+    }
+}
