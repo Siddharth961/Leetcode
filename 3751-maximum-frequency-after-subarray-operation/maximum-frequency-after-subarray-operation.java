a3d344936fbecd22553b685d4b2deace03e8bc62
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-26 16:58:57 +0530

    Time: 56 ms (21.55%) | Memory: 60.8 MB (55.60%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
@@ -2,37 +2,13 @@ class Solution {
     public int maxFrequency(int[] nums, int k) {
         HashMap<Integer, Integer> mp = new HashMap<>();
 
-        // int lost_k = 0;
         int total_k = 0;
-        // int max_freq = 0;
 
         int ans = 0;
 
         for(int i : nums) if(i == k) total_k++;
 
-        // for(int i : nums){
-
-        //     if(i == k){
-        //         lost_k++;
-        //     }
-        //     else{
-        //         mp.put(i, mp.getOrDefault(i, 0) + 1);
-
-        //         max_freq = Math.max(max_freq, mp.get(i) );
-        //     }
-
-        //     int val = max_freq + total_k - lost_k;
-        //     System.out.println(val +" "+ max_freq + " " + total_k + " " + lost_k);
-
-        //     if( max_freq - lost_k <= 0){
-        //         lost_k = 0;
-        //         mp.clear();
-        //     }
-
-
-        //     ans = Math.max(ans, val);
-
-        // }
+        
 
         for(int i=1; i<=50; i++){
             ans = Math.max( ans, check(k, i, total_k, nums) );
