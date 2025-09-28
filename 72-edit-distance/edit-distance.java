6c648fb13f6c7ab69f2d89c413a0a1dc4adc81e2
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-28 17:36:45 +0530

    Time: 7 ms (4.32%) | Memory: 45.1 MB (46.83%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,60 @@
+class Solution {
+    int[][]dp ;
+    public int minDistance(String word1, String word2) {
+
+        dp = new int[word1.length()][word2.length()];
+        return get_ans( 0, 0, word1, word2);
+    }
+
+    public int get_ans(int i, int j,  String word1, String word2){
+
+        if(j == word2.length() ){
+            int ops = word1.length() - i ;
+
+            return ops;
+
+        }
+
+        if(i == word1.length() ){
+            int ops = word2.length() - j;
+            return ops;
+        }
+
+        if(dp[i][j] != 0) return dp[i][j];
+
+        int ans = Integer.MAX_VALUE;
+
+        // insert before this index
+        int val = get_ans(i, j+1, word1, word2);
+
+        ans = Math.min(ans, val+1);
+
+        // delete / skip this elment
+
+        val = get_ans(i+1, j,  word1, word2 );
+        ans = Math.min(ans, val+1);
+
+        // if same elment
+        if(word1.charAt(i) == word2.charAt(j)){
+
+            val = get_ans(i+1, j+1, word1, word2);
+            ans = Math.min(ans, val); // no operation needed
+        }
+
+        else{
+            // replace
+            val = get_ans(i+1, j+1, word1, word2);
+            ans = Math.min(ans, val+1); //  operation needed
+            
+        }
+
+        if(ans == Integer.MAX_VALUE) ans--;
+
+        dp[i][j] = ans;
+
+        return ans;
+
+
+        
+    }
+}
