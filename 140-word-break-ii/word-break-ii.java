0abc2ba190c5c685a902eb6592f8faf616a0493c
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-29 02:15:13 +0530

    Time: 1 ms (92.95%) | Memory: 41.6 MB (89.27%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,54 +1,82 @@
 class Solution {
-    List<String>ans = new ArrayList<>();
-    List<String> asf = new ArrayList<>();
-
+    List<String> ans;
     public List<String> wordBreak(String s, List<String> wordDict) {
-        HashSet<String> st = new HashSet<>(wordDict);
 
-        get_ans(0, 0, s, st);
+        ans = new LinkedList<>();
+        Node root = new Node();
+
+        for(String word : wordDict) makeTrie(0, root, word);
+                
+        travel(0, root, root, s, new LinkedList<>());
+
         return ans;
-
     }
 
-    public void get_ans(int idx, int left, String s,  HashSet<String>st){
+    public void travel(int idx, Node curr, Node root, String s, List<Integer>spaces){
 
         if(idx == s.length()){
-
-            if(left==idx){
-                
-                make_ans();
+            if(curr == root){
+                ans.add( add_ans(spaces, s) );
+                // System.out.println(spaces);
             }
-            return;
+
+            return ;
         }
 
-        String ssf = s.substring(left, idx+1);
-        
-        
-        if(st.contains( ssf )){
+        char c = s.charAt(idx);
 
-            asf.add(ssf);
-            
-            get_ans(idx+1, idx+1, s, st);
+        Node child = curr.children[c-'a'];
 
-            asf.remove( asf.size()-1);
+        if( child == null) return;
+
+        if(child.end == true){
+            spaces.add(idx);
+            travel(idx+1, root, root, s, spaces);
+            spaces.remove(spaces.size()  - 1);
         }
+        travel(idx+1, child, root, s, spaces);
 
-        get_ans(idx+1, left, s, st);
     }
 
-    public void make_ans(){
+    public String add_ans(List<Integer>spaces, String s){
 
-        if(asf.size()==0) return;
-
+        int idx = 0;
         StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length() ; i++){
 
-        for(String s : asf){
-            sb.append(s);
-            sb.append(' ');
+            sb.append( s.charAt(i) );
+
+            if(i!=s.length()-1 && i == spaces.get(idx)){
+                sb.append(' ');
+                idx++;
+            }
+
         }
 
-        sb.deleteCharAt( sb.length()-1 );
+        return sb.toString();
+    }
 
-        ans.add( sb.toString() );
+    public void makeTrie(int idx, Node root, String s){
+        if(idx == s.length() ) return;
+
+        int val = s.charAt(idx) - 'a';
+
+        if(root.children[val] == null ) root.children[val] = new Node();
+
+        Node child = root.children[val];
+
+        if(idx == s.length() - 1) child.end = true;
+
+        makeTrie(idx+1, child, s);
+    }
+
+    public class Node{
+        boolean end;
+        Node[]children;
+
+        Node(){
+            end = false;
+            children = new Node[26];
+        }
     }
 }
