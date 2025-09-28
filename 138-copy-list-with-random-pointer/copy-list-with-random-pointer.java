31d0d17109f882c99062327f49912c72b595a1a0
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-09-28 16:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (42.49%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -14,63 +14,41 @@ class Node {
 */
 
 class Solution {
+    HashMap<Node, Node> lookup;
+
     public Node copyRandomList(Node head) {
-        if(head==null)return null;
-        Node chead = new Node(100);
-        chead = deepcopy(head,chead);
-        // System.out.println(chead.val);
 
-        Node thead = new Node(100);
-        Node t = thead;
-        Node ptr1 = head;
-        Node ptr2 = chead;
+        lookup = new HashMap<>();
 
-        while(ptr1!=null){
-            t.next  = ptr1;
-            ptr1 = ptr1.next;
-            t = t.next;
+        Node n_head = makeList(head);
 
-            t.next = ptr2;
-            ptr2 = ptr2.next;
-            t = t.next;
-        }
+        populate_random(head);
 
-        ptr1=head;
-
-        while(ptr1!=null){
-
-            if(ptr1.random==null)ptr1.next.random = null;
-            else ptr1.next.random = ptr1.random.next;
-            ptr1 = ptr1.next.next;
-        }
-
-        System.out.println(head.next.val);
-        ptr1 = head;
-        ptr2 = chead;
-        while(ptr2!=null){
-            ptr1.next = ptr2.next;
-            ptr1 = ptr1.next;
-            if(ptr1 == null)break;
-            ptr2.next = ptr1.next;
-            ptr2 = ptr2.next;
-        }
-
-        return chead;
+        return n_head;
+        
     }
 
-    public Node deepcopy(Node head, Node chead){
-        Node temp = chead;
-        Node ptr  = head;
+    public Node makeList(Node head){
+        if(head == null) return null;
 
-        while(ptr!=null){
-            Node dummy = new Node(ptr.val);
-            temp.next = dummy;
-            temp = dummy;
-            ptr = ptr.next;
-        }
-        // System.out.println(temp.val);
+        Node new_node = new Node(head.val);
 
-        return chead.next;
+        lookup.put(head, new_node);
 
+        new_node.next = makeList(head.next);
+
+        return new_node;
     }
+
+    public void populate_random(Node head){
+
+        if(head == null) return;
+
+        Node curr = lookup.get(head);
+
+        if(head.random != null)curr.random = lookup.get( head.random );
+
+        populate_random(head.next);
+    }
+
 }
