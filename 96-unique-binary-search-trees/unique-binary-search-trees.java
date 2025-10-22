225f5b245c999348725b429f4b7de679595aad42
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-10-22 19:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (38.29%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,25 +1,30 @@
 class Solution {
     public int numTrees(int n) {
-        return get_ans(1, n);
+        int[]dp = new int[n+1];
+        return get_ans(1, n, dp);
     }
 
-    public int get_ans(int st, int en){
+    public int get_ans(int st, int en, int[]dp){
 
         if(st > en) return 1;
 
         if(st == en) return 1;
 
+        if(dp[en-st+1] != 0) return dp[en-st+1];
         int ans = 0;
+
 
         for(int i=st; i<=en; i++){
             
-            int val = get_ans( st, i-1) * get_ans(i+1, en);
+            int val = get_ans( st, i-1, dp) * get_ans(i+1, en, dp);
 
             // if(i == 2 && st == 1 && en == 3) System.out.println(val);
 
             ans += val;
 
         }
+
+        dp[en-st+1] = ans;
 
         return ans;
     }
