9ed9bbf22c8444cb005f8169f50aaac39252b3cc
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-10-23 14:53:10 +0530

    Time: 1 ms (99.42%) | Memory: 45.4 MB (80.19%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,29 +1,35 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> tri) {
-        int r = tri.size();
-        int c = tri.get(r-1).size();
+    public int minimumTotal(List<List<Integer>> triangle) {
 
-        int[][]dp = new int[r][c];
-        for(int[]arr : dp)Arrays.fill(arr, Integer.MAX_VALUE);
+        dfs(1, triangle);
 
-        return get_ans(tri, 0, 0, dp);
+        List<Integer> al = triangle.get(triangle.size() - 1);
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i : al) min = Math.min(i, min);
+
+        return min;
     }
 
-    public int get_ans(List<List<Integer>> tri, int r, int c, int[][]dp){
+    public void dfs(int row,  List<List<Integer>> triangle){
+
+        if(row == triangle.size()) return;
+
+        List<Integer> prev = triangle.get(row-1);
+        List<Integer> curr = triangle.get(row);
+
+        for(int i=0; i<curr.size(); i++){
+
+            int val = Integer.MAX_VALUE;
+
+            if(i > 0 ) val = prev.get(i-1);
+            if(i < prev.size()) val = Math.min( val, prev.get(i));
+
+            curr.set(i, curr.get(i) + val);
+        }
+
+        dfs(row + 1, triangle);
         
-
-        if(r==tri.size()) return 0;
-        if(dp[r][c] != Integer.MAX_VALUE) return dp[r][c];
-
-        List<Integer> al = tri.get(r);
-                
-
-        int x = get_ans(tri, r+1, c, dp);
-        int y = get_ans(tri, r+1, c+1, dp);
-
-        int ans  = al.get(c) + Math.min(x , y);    
-         
-        dp[r][c] = ans;
-        return ans; 
     }
 }
