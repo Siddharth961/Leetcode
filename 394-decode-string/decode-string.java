5a6083a4b78cad6dd35fd4101becd974cc77adcf
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2025-11-22 20:07:32 +0530

    Time: 3 ms (19.32%) | Memory: 43.1 MB (6.61%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,33 +1,78 @@
 class Solution {
-    int i = 0;
     public String decodeString(String s) {
-        StringBuilder sb = new StringBuilder();
-        int num = 0;
-        String temp = "";
+        
+        Stack<String> st = new Stack<>();
 
-        while(i<s.length()){
+        for(int i=0; i<s.length(); i++){
+
             char c = s.charAt(i);
-            i++;
-            if(c=='['){
-                temp = decodeString(s);
-                for(int j=0; j<num; j++){
-                    sb.append(temp);
+
+            // if( c == '[') continue;
+
+            if(c >= '0' && c <='9'){
+
+                if( isSame(st, c) ){
+                    st.push( st.pop() + c );
+                }
+                else st.push(c+"");
+            }
+
+            else if( c != ']'){
+                st.push(c+"");
+            }
+
+            else {
+
+                StringBuilder sb = new StringBuilder();
+                String str = "";
+
+                while(st.peek().equals("[") == false){
+                    str = st.pop() + str;
                 }
 
-                num = 0;
-                
-                            
+                st.pop(); // remove open bracket
+
+                // String str = sb.reverse().toString(); 
+                String num = st.pop();
+
+                String eval = parse(num, str);
+                st.push(eval);
             }
 
-            else if(c==']') break;
+            // System.out.println(st);
+        }
 
-            else if(Character.isAlphabetic(c)) sb.append(c);
-            else{
-                num = num * 10 + c - '0';
-            }
+        StringBuilder sb = new StringBuilder();
+
+        for(String str: st) sb.append(str);
+
+        return sb.toString();
+    }
+
+    public boolean isSame(Stack<String> st, char c ){
+
+        if( st.size() == 0) return false;
+
+        char c1 = st.peek().charAt(0);
+
+        if( (c >= '0' && c <='9') && (c1 >= '0' && c1 <='9') ) return true;
+
+        // if( (c >= 'a' && c <='z') && (c1 >= 'a' && c1 <='z') ) return true;
+
+        return false;
+    }
+
+    public String parse(String num, String s){
+
+        int n = Integer.parseInt(num);
+
+        StringBuilder sb = new StringBuilder();
+
+        while(n > 0){
+            sb.append(s);
+            n--;
         }
 
         return sb.toString();
-
     }
 }
