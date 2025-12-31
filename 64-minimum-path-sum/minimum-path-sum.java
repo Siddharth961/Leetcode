fe927d52728aaeda1a5a6f6111090a25d03a3031
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2026-01-01 02:27:06 +0530

    Time: 1 ms (99.68%) | Memory: 50.5 MB (19.07%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,53 +1,26 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n= grid[0].length;
-        int[][]dp = new int[m][n];
-        int val = Integer.MAX_VALUE;
-
-        // for(int i=m-1; i>=0; i--){
-
-        //     for(int j=n-1; j>=0; j--){
-
-        //         if(i==m-1 && j == n-1){
-        //             dp[i][j] = grid[i][j];
-        //             continue;
-        //         }
-                
-                
-        //         val = Integer.MAX_VALUE;
-        //         if(i+1<m){
-        //             val = Math.min(val, dp[i+1][j]);
-        //         }
-        //         if(j+1 < n){
-        //             val = Math.min(val, dp[i][j+1]);
-        //         }
-
-        //         dp[i][j] = val + grid[i][j];
         
-        //     }
-        // }
+        int[][]dp = new int[grid.length][grid[0].length];
 
-        // return dp[0][0];
+        for(int[]arr : dp) Arrays.fill(arr, -1);
 
         return get_ans(0, 0, grid, dp);
     }
 
-    public int get_ans(int i, int j, int[][]grid, int[][]dp){
-        if(i==grid.length || j==grid[0].length) return Integer.MAX_VALUE;
+    public int get_ans(int r, int c, int[][]grid, int[][]dp){
+        if(r == grid.length || c == grid[0].length) return Integer.MAX_VALUE;
 
-        if(dp[i][j]!=0) return dp[i][j];
+        if(dp[r][c] != -1) return dp[r][c];
 
-        int a = get_ans(i+1, j, grid, dp);
-        int b = get_ans(i, j+1, grid, dp);
-        
-        int ans = Math.min(a,b) ;
-        if(ans == Integer.MAX_VALUE) ans = 0 ;
+        int ans = get_ans(r+1, c, grid, dp);
 
-        ans += grid[i][j];
-        
-        dp[i][j] = ans;
+        ans = Math.min(ans, get_ans(r, c+1, grid, dp));
+
+        if(ans == Integer.MAX_VALUE) ans = 0;
+        ans += grid[r][c];
+
+        dp[r][c] = ans;
         return ans;
-
     }
 }
