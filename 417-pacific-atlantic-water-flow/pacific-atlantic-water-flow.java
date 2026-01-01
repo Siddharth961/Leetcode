a3c524cbe1fe12a609b11f0bd921235558038f44
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2026-01-01 14:47:38 +0530

    Time: 10 ms (33.77%) | Memory: 47.4 MB (53.85%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,99 +1,67 @@
-
 class Solution {
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    public List<List<Integer>> pacificAtlantic(int[][] grid) {
+        
+        Queue<int[]> q = new LinkedList<>();
 
-        int r = heights.length;
-        int c = heights[0].length;
-        boolean[][][]dp = new boolean[r][c][2];
-        boolean[][]visited = new boolean[r][c];
-
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-
-                travel(i, j, Integer.MAX_VALUE, heights, dp, visited);
-            }
+        for(int i=0; i<grid.length; i++){
+            q.add(new int[]{i, 0});
         }
+        for(int i=0; i<grid[0].length; i++){
+            q.add(new int[]{0, i});
+        }
+
+        boolean[][]pacific = bfs(q, grid);
+
+        for(int i=0; i<grid.length; i++){
+            q.add(new int[]{i, grid[0].length - 1});
+        }
+        for(int i=0; i<grid[0].length; i++){
+            q.add(new int[]{grid.length - 1, i});
+        }
+
+        boolean[][]atlantic = bfs(q, grid);
 
         List<List<Integer>> ans = new LinkedList<>();
 
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
 
-                if(dp[i][j] == null) continue;
-
-                // System.out.println(i +" - "+ j + " : "+  dp[i][j][0] + " " + dp[i][j][1]);
-
-                if(  dp[i][j][0] && dp[i][j][1] ){
-                    List<Integer> ll = new LinkedList<>();
+                if(pacific[i][j] && atlantic[i][j]){
+                    List<Integer>ll  = new LinkedList<>();
                     ll.add(i);
                     ll.add(j);
-
                     ans.add(ll);
                 }
             }
         }
 
         return ans;
+
+
     }
 
-    public boolean[] travel( int r, int c, int prev, int[][]grid, boolean[][][]dp, boolean[][]visited){
+    public  boolean[][] bfs(Queue<int[]> q , int[][]grid){
 
-        boolean[]ans = new boolean[2];
+        boolean[][]visited = new boolean[grid.length][grid[0].length];
 
-        if( r < 0 || c < 0 ){
-            ans[0] = true;
-            ans[1] = false;
-            // if(r == -1 && c== 2){
-            //     System.out.println("aa " +  ans[0] + " " + ans[1]);
-            // }
-            return ans; // only top left reached
-        }
-        if( r == grid.length || c == grid[0].length ){
-            ans[0] = false;
-            ans[1] = true;
-            return ans; // only bottom right reached
-        }
-        // System.out.println(11);
+        while(q.size() > 0){
 
+            int[]point = q.remove();
+            int r = point[0];
+            int c = point[1];
 
-        if(prev < grid[r][c] || visited[r][c] == true) return ans; // invalid access
+            if(  visited[r][c] ) continue;
 
+            visited[r][c] = true;
+            // ans[r][c]++;
 
-        if(dp[r][c][0] == true && dp[r][c][1] == true){
-            // if(r==1 && c==2){
-            //     System.out.println("dahi " + dp[r][c][0] + dp[r][c][1]);
-            // }
-            return dp[r][c];
-        }
-        visited[r][c] = true;
-
-        boolean[]response = null;
-
-        for(int i=0; i<4; i++){
-            if(i==0)response = travel( r-1, c, grid[r][c], grid, dp, visited);
-            if(i==1)response = travel( r, c+1, grid[r][c], grid, dp, visited);
-            if(i==2)response = travel( r+1, c, grid[r][c], grid, dp, visited);
-            if(i==3)response = travel( r, c-1, grid[r][c], grid, dp, visited);
-
-            // if(r == 1 && c== 0){
-            //     System.out.println( response[0] + " " + response[1]);
-            // }
-
-            ans[0] = ans[0] || response[0];
-            ans[1] = ans[1] || response[1];
+            if(r+1 < grid.length && grid[r+1][c] >= grid[r][c]) q.add(new int[]{r+1, c});
+            if(c+1 < grid[0].length && grid[r][c+1] >= grid[r][c]) q.add(new int[]{r, c+1});
+            if(r-1 >= 0 && grid[r-1][c] >= grid[r][c]) q.add(new int[]{r-1, c});
+            if(c-1 >= 0 && grid[r][c-1] >= grid[r][c]) q.add(new int[]{r, c-1});
         }
 
-        //  if(r == 0 && c== 2){
-        //         System.out.println("ans " + ans[0] + " " + ans[1] +" -- " + prev);
-        //     }
+        return visited;
 
-        dp[r][c] = new boolean[2];
-        dp[r][c][0] = ans[0];
-        dp[r][c][1] = ans[1];
-         
-
-        visited[r][c] = false;
-
-        return ans;
     }
 }
