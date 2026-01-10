098c609c58d5aca2d507a9a763a229a6c30c6e3b
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2026-01-10 19:31:44 +0530

    Time: 50 ms (17.95%) | Memory: 42.7 MB (58.23%) - LeetSync

---
+++ 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAllOneMultiple(int k) {
+        // if(k % 2 == 0 || k % 5 == 0){
+        //     return -1;
+        // }
+        
+        int digits = 1;
+        int rem = 0;
+
+        while(digits <= k){
+
+            rem = ((rem * 10) + 1) % k;
+
+            if(rem == 0) return digits;
+            digits++;
+
+        }
+
+        return -1;
+    }
+}
