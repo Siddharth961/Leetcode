89c45ffc3aed3c0cb091aa10b632051fa4b1c9d9
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2026-03-14 16:33:20 +0530

    Time: 124 ms (30.83%) | Memory: 48.3 MB (28.75%) - LeetSync

---
+++ 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.java
@@ -0,0 +1,47 @@
+class Solution {
+    public long minCost(String s, int encCost, int flatCost) {
+        return calc(0, s.length()-1, s, encCost, flatCost);
+    }
+
+    public long calc(int st, int en, String s, int enc, int flat){
+
+        long cost = 0;
+
+        int ones = 0;
+
+        int len = en-st+1;
+        int mid = (st + en) / 2;
+
+        for(int i=st; i<=en; i++){
+            if(s.charAt(i) == '1'){
+
+                ones++;
+            }
+        }
+
+        if(ones != 0){
+            // only split if there is atleast single 1 otherwise both splits will still have all zeroes only
+
+            cost = (long) len * ones * enc;
+
+            if(len % 2 == 0){
+
+                long a = calc(st, mid, s, enc, flat);
+                long b = calc(mid+1, en, s, enc, flat);
+
+                // if(cost < 0) cost = a+b;
+
+                cost = Math.min(a+b, cost);
+                
+            }
+        }
+        else cost = flat;
+
+        // if(cost < 0) System.out.println('1');
+
+        return cost;
+    }
+}
+
+// there should be at max a single 1 in a segment
+// 1 should be there only till L * enc <= flatcost ??
