d9438ee548a97fa9c5bb31614277d2e83789dde4
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2026-04-12 16:29:44 +0530

    Time: 298 ms (50.00%) | Memory: 92.5 MB (0.00%) - LeetSync

---
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.java
@@ -0,0 +1,43 @@
+class Solution {
+    HashSet<Integer> primes = new HashSet<>();
+    public int minOperations(int[] nums) {
+
+        int ops = 0;
+
+        for(int i=0; i<nums.length; i++){
+
+            if( i % 2 == 0 ){
+
+                // if(isPrime(nums[i]) ) continue;
+
+                while( isPrime(nums[i] ) == false){
+                    nums[i]++;
+                    ops++;
+                }
+            }
+
+            else{
+                while( isPrime(nums[i] ) == true){
+                    nums[i]++;
+                    ops++;
+                }
+            }
+        }
+
+        return ops;
+        
+    }
+
+    public boolean isPrime(int val){
+
+        if(val == 1) return false;
+
+        
+
+        for(int i=2; i*i <= val; i++){
+            if(val % i == 0) return false;
+        }
+
+        return true;
+    }
+}
