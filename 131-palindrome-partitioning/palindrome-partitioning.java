f8d15c3087b5d7da6abaf315b209e4da3427da96
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2026-07-02 18:34:18 +0530

    Time: 9 ms (25.73%) | Memory: 65.1 MB (73.78%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -6,36 +6,30 @@ class Solution {
     }
 
     public void travel(int idx, String s, List<String>ssf ){
-
-
         if(idx == s.length()){
             ans.add( new ArrayList<>(ssf) );
-            return;
         }
 
         for(int end = idx; end < s.length(); end++){
+
             String part = s.substring(idx, end+1);
 
-            if( (isPalindrome(part)) ){
-
-                // valid partition
+            if( isPalindrome(part) ){
 
                 ssf.add(part);
                 travel(end+1, s, ssf);
-                ssf.remove( ssf.size() - 1);
+                ssf.remove(ssf.size() - 1);
             }
         }
-
     }
 
     public boolean isPalindrome(String s){
-
         int i = 0;
-        int j = s.length() - 1;
+        int j = s.length()-1;
 
         while(i < j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-
+            if(s.charAt(i) != s.charAt(j) ) return false;
+            
             i++;
             j--;
         }
