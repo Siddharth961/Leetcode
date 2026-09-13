a7d8a466d8c5ff4365503258807e39c1b54146a4
Siddharth Jain <99834650+Siddharth961@users.noreply.github.com>
2026-09-13 15:07:21 +0530

    Time: 53 ms (94.51%) | Memory: 79.7 MB (17.20%) - LeetSync

---
+++ 3993-transform-binary-string-using-subsequence-sort/transform-binary-string-using-subsequence-sort.java
@@ -0,0 +1,60 @@
+class Solution {
+    boolean debug = false;
+
+    public boolean[] transformStr(String s, String[] strs) {
+
+        boolean[]ans = new boolean[strs.length];
+
+        for(int i=0; i<ans.length; i++){
+            // dp = new int[s.length()];
+            // Arrays.fill(dp, -1);
+            if(i==0){
+                debug = true;
+                System.out.println(strs[i]);
+            }
+            ans[i] = get_ans( s.toCharArray(), strs[i].toCharArray());
+        }
+
+        return ans;
+    }
+
+    public boolean get_ans( char[] s, char[] t){
+
+        int one_zero = 0;
+        int zero_one = 0;
+        int one_mark = 0;
+
+        for(int i=0; i<s.length; i++){
+
+            if(s[i] == '0' && one_zero > 0){
+                s[i] = '1';
+                one_zero--;
+                i--;
+                continue;
+            }
+
+            if( s[i] == '1' && t[i] == '0'){
+                one_zero++;
+            }
+            if(s[i] == '1' && t[i] == '?'){
+                one_mark++;
+            }
+
+            if(s[i] == '0' && t[i] == '1'){
+
+                if(one_zero > 0){
+                    one_zero--;
+                }
+
+                else if(one_mark > 0) one_mark--;
+                    
+                else return false;
+                
+            }
+        }
+
+        return one_zero == 0;     
+
+        
+    }
+}
